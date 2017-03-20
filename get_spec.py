4bdac10b9ce9e896daa1ea79826d1f2db16f8bf5
Molly Peeples <molly@stsci.edu>
2017-03-20 08:14:49 -0400

    beginning of setting up pipeline for generating lots of misty spectra

--- get_spec.py
+++ get_spec.py
@@ -1,17 +1,17 @@
 import trident
-import numpy as np 
+import numpy as np
 import yt
-import analysis 
+import analysis
 
-def get_spec(ds, halo_center, rho_ray): 
+def get_spec(ds, halo_center, rho_ray):
 
-    proper_box_size = ds.get_parameter('CosmologyComovingBoxSize') / ds.get_parameter('CosmologyHubbleConstantNow') * 1000. # in kpc 
+    proper_box_size = ds.get_parameter('CosmologyComovingBoxSize') / ds.get_parameter('CosmologyHubbleConstantNow') * 1000. # in kpc
     line_list = ['H', 'O', 'C', 'N', 'Si'] 
 
-    # big rectangle box 
-    ray_start = [halo_center[0]-250./proper_box_size, halo_center[1]+71./proper_box_size, halo_center[2]-71./proper_box_size] 
-    ray_end =   [halo_center[0]+250./proper_box_size, halo_center[1]+71./proper_box_size, halo_center[2]-71./proper_box_size] 
- 
+    # big rectangle box
+    ray_start = [halo_center[0]-250./proper_box_size, halo_center[1]+71./proper_box_size, halo_center[2]-71./proper_box_size]
+    ray_end =   [halo_center[0]+250./proper_box_size, halo_center[1]+71./proper_box_size, halo_center[2]-71./proper_box_size]
+
     ray = trident.make_simple_ray(ds, start_position=ray_start,
                                   end_position=ray_end,
                                   data_filename="ray.h5",
@@ -21,44 +21,17 @@ def get_spec(ds, halo_center, rho_ray):
     sg = trident.SpectrumGenerator(lambda_min=1020, lambda_max=1045, dlambda=0.002)
     trident.add_ion_fields(ds, ions=['C IV', 'O VI','H I','Si III'])
 
-    sg.make_spectrum(ray, lines=line_list) 
-    sg.plot_spectrum('spec_final.png') 
-    sg.save_spectrum('spec_final.txt') 
-    
-    # this makes for convenient field extraction from the ray 
-    ad = ray.all_data() 
-    dl = ad["dl"] # line element length 
+    sg.make_spectrum(ray, lines=line_list)
+    sg.plot_spectrum('spec_final.png')
+    sg.save_spectrum('spec_final.txt')
 
-    out_dict = {'nhi':np.sum(ad['H_number_density']*dl), 'no6':np.sum(ad['O_p5_number_density']*dl), 'nsi3':np.sum(ad['Si_p2_number_density']*dl)} 
-    print np.sum(ad['H_number_density']*dl) 
-    print np.sum(ad['O_p5_number_density']*dl) 
-    print np.sum(ad['Si_p2_number_density']*dl) 
+    # this makes for convenient field extraction from the ray
+    ad = ray.all_data()
+    dl = ad["dl"] # line element length
 
-#yt : [INFO     ] 2017-02-16 21:11:32,811 Creating O_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:32,860 Creating O_p1_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:32,902 Creating O_p2_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:32,948 Creating O_p3_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:32,991 Creating O_p4_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,034 Creating O_p5_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,044 Creating C_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,083 Creating C_p1_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,124 Creating C_p2_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,161 Creating C_p3_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,171 Creating N_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,243 Creating N_p1_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,282 Creating N_p2_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,319 Creating N_p3_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,363 Creating N_p4_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,402 Creating Si_p1_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,479 Creating Si_p2_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,487 Creating Si_p3_number_density from ray's density, temperature, metallicity.
-#yt : [INFO     ] 2017-02-16 21:11:33,561 Creating Si_p11_number_density from ray's density, temperature, metallicity.
+    out_dict = {'nhi':np.sum(ad['H_number_density']*dl), 'no6':np.sum(ad['O_p5_number_density']*dl), 'nsi3':np.sum(ad['Si_p2_number_density']*dl)}
+    print np.sum(ad['H_number_density']*dl)
+    print np.sum(ad['O_p5_number_density']*dl)
+    print np.sum(ad['Si_p2_number_density']*dl)
 
-    return ray, out_dict 
-
-
-   
-
-
-
-
+    return ray, out_dict
---
+++ sample_misty_spectra.py
@@ -0,0 +1,86 @@
+import trident
+import numpy as np
+import yt
+import MISTY
+
+import getpass
+
+from math import pi
+
+def get_ray_endpoints(ds, halo_center, **kwargs):
+    '''
+    returns ray_start and ray_end for a ray with a given
+    impact parameter along a given axis, with either specified
+    or random angle
+    '''
+    axis = kwargs.get("axis", 2)
+    impact = kwargs.get("impact", 100.)
+    angle = kwargs.get("angle", 2*pi*np.random.uniform())
+    dl = kwargs.get("dl", 500.)  # pathlength of ray
+    proper_box_size = ds.get_parameter('CosmologyComovingBoxSize') / ds.get_parameter('CosmologyHubbleConstantNow') * 1000. # in kpc
+
+    ray_start = np.zeros(3)
+    ray_end = np.zeros(3)
+    if axis == "x" or axis == 0:
+        axis = 0
+        dx = 0
+        dy = impact * np.cos(angle)
+        dz = impact * np.sin(angle)
+    elif axis == "y" or axis == 1:
+        axis = 1
+        dx = impact * np.cos(angle)
+        dy = 0
+        dz = impact * np.sin(angle)
+    elif axis == "z" or axis == 2:
+        axis = 2
+        dx = impact * np.cos(angle)
+        dy = impact * np.sin(angle)
+        dz = 0
+    else:
+        print "---> your axis does not make any sense :-("
+
+    delta = [dx,dy,dz]
+    for ax in (0,1,2):
+        ray_start[ax] = halo_center[ax] + delta[ax]/proper_box_size
+        ray_end[ax] = halo_center[ax] + delta[ax]/proper_box_size
+    ray_start[axis] -= 0.5*dl/proper_box_size
+    ray_end[axis] += 0.5*dl/proper_box_size
+
+    return np.array(ray_start), np.array(ray_end)
+
+def generate_random_rays(ds, halo_center, **kwargs):
+    '''
+    generate some random rays
+    '''
+    low_impact = kwargs.get("low_impact", 10.)
+    high_impact = kwargs.get("high_impact", 200.)
+    Nrays = kwargs.get("Nrays",50)
+    line_list = kwargs.get("line_list", ['H I','Si III','C IV', 'O VI'])
+
+    ## for now, assume all are z-axis
+    axis = "z"
+    impacts = np.random.uniform(low=low_impact, high=high_impact, size=Nrays)
+    angles = np.random.uniform(low=0, high=2*pi, size=Nrays)
+    out_ray_basename = ds.basename + "_ray_" + axis
+
+    for i in Nrays:
+        out_ray_name = out_ray_basename + "imp"+"{:.1f}".format(impacts[i]) + \
+                        "_ang"+"{:.2f}".format(angles[i])+".h5"
+        rs, re = get_ray_endpoints(ds, halo_center, impact=impacts[i], angle=angles[i], axis=axis)
+        rs = ds.arr(rs, "code_length")
+        re = ds.arr(re, "code_length")
+        ray = ds.ray(rs, re)
+        ray.save_as_dataset(out_ray_name)
+        triray = trident.make_simple_ray(ds, start_position=rs,
+                                  end_position=re,
+                                  data_filename=out_ray_name,
+                                  lines=line_list,
+                                  ftype='gas')
+
+        ray_start = triray.light_ray_solution[0]['start']
+        ray_end = triray.light_ray_solution[0]['end']
+        filespecout_base = 'spectrum_'+triray.light_ray_solution[0]['filename']
+        print ray_start, ray_end, filespecout_base
+
+        hdulist = MISTY.write_header(triray,start_pos=ray_start,end_pos=ray_end,
+                      lines=line_list, author=getpass.getuser())
