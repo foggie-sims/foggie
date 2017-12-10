c0e1589c8f2abe1a8257274e04ccc348097b2888
Molly Peeples <molly@stsci.edu>
2017-12-10 12:22:06 -0500

    proper_box_size fixed

--- get_background_density.py
+++ get_background_density.py
@@ -3,13 +3,14 @@
 import yt
 import numpy as np
 
-def get_background_density(ds): 
+from get_proper_box_size import get_proper_box_size
+
+def get_background_density(ds):
 
     ad = ds.all_data()
-    proper_box_size = ds.arr(ds.get_parameter('CosmologyComovingBoxSize'), 'Mpc') / ds.get_parameter('CosmologyHubbleConstantNow') 
+    proper_box_size = get_proper_box_size(ds)
     baryon_mass, particle_mass = ad.quantities.total_quantity(["cell_mass", "particle_mass"])
     BoxTotalMass = (baryon_mass + particle_mass).in_units('Msun')
     BoxDensity = BoxTotalMass / (proper_box_size)**3
 
-    return BoxDensity 
-
+    return BoxDensity
