cf09ba5a76da93eaf2b86fb79987305acb2dd07d
Molly Peeples <molly@stsci.edu>
2018-01-01 11:23:12 -0500

    default search radius is 100kpc and also passable

--- get_halo_center.py
+++ get_halo_center.py
@@ -1,12 +1,12 @@
 import yt
 import numpy as np
 
-def get_halo_center(ds, center_guess):
-
+def get_halo_center(ds, center_guess, **kwargs):
     # returns a list of the halo center coordinates
+    kwargs.get("radius", 100.)  # search radius in kpc
 
     # now determine the location of the highest DM density, which should be the center of the main halo
-    ad = ds.sphere(center_guess, (200., 'kpc')) # extract a sphere centered at the middle of the box
+    ad = ds.sphere(center_guess, (radius, 'kpc')) # extract a sphere centered at the middle of the box
     x,y,z = np.array(ad["x"]), np.array(ad["y"]), np.array(ad["z"])
     dm_density =  ad['Dark_Matter_Density']
     imax = (np.where(dm_density > 0.9999 * np.max(dm_density)))[0]
