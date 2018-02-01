25fe045bb2e82776ea60bd7e11ff10c74ed5cfd8
Molly Peeples <molly@stsci.edu>
2018-02-01 14:56:17 -0500

    now in function format

--- manage_path_names.py
+++ manage_path_names.py
@@ -8,10 +8,14 @@ username = getpass.getuser()
 import socket
 hostname = socket.gethostname()
 
-if username == "molly" and hostname == "dhumuha-2.local":
-    foggie_dir = "/Users/molly/foggie/"
-    output_dir = "/Users/molly/Dropbox/foggie-collab/"
+def get_path_names():
+    foggie_dir, output_dir = ""
+    if username == "molly" and hostname == "dhumuha-2.local":
+        foggie_dir = "/Users/molly/foggie/"
+        output_dir = "/Users/molly/Dropbox/foggie-collab/"
 
-if username == "molly" and hostname == "oak":
-    foggie_dir = "/astro/simulations/FOGGIE/"
-    output_dir = "/Users/molly/Dropbox/foggie-collab/"
+    if username == "molly" and hostname == "oak":
+        foggie_dir = "/astro/simulations/FOGGIE/"
+        output_dir = "/Users/molly/Dropbox/foggie-collab/"
+
+    return foggie_dir, output_dir
