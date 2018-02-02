2d5b76979efed30916872b0fb6a0e67b45136fea
Molly S. Peeples <molly@stsci.edu>
2018-02-02 15:22:23 -0500

    cleaned up

--- manage_path_names.py
+++ manage_path_names.py
@@ -3,18 +3,17 @@ try to dynamically figure out relevant paths
 '''
 
 import getpass
-username = getpass.getuser()
-
 import socket
-hostname = socket.gethostname()
 
 def get_path_names():
+    username = getpass.getuser()
+    hostname = socket.gethostname()
     foggie_dir, output_dir = ""
     if username == "molly" and hostname == "dhumuha-2.local":
         foggie_dir = "/Users/molly/foggie/"
         output_dir = "/Users/molly/Dropbox/foggie-collab/"
 
-    if username == "molly" and hostname == "oak":
+    if username == "molly" and hostname == "oak.stsci.edu":
         foggie_dir = "/astro/simulations/FOGGIE/"
         output_dir = "/Users/molly/Dropbox/foggie-collab/"
 
