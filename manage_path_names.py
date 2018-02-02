ad2ac26fea3bae6ca186a89edc5869d83a22755c
Molly S. Peeples <molly@stsci.edu>
2018-02-02 15:31:34 -0500

    cleaner test more scalable to e.g., pleiades

--- manage_path_names.py
+++ manage_path_names.py
@@ -9,11 +9,11 @@ def get_path_names():
     username = getpass.getuser()
     hostname = socket.gethostname()
     foggie_dir, output_dir = "", ""
-    if username == "molly" and hostname == "dhumuha-2.local":
+    if username == "molly" and "dhumuha" in hostname:
         foggie_dir = "/Users/molly/foggie/"
         output_dir = "/Users/molly/Dropbox/foggie-collab/"
 
-    if username == "molly" and hostname == "oak.stsci.edu":
+    if username == "molly" and "oak" in hostname:
         foggie_dir = "/astro/simulations/FOGGIE/"
         output_dir = "/Users/molly/Dropbox/foggie-collab/"
 
