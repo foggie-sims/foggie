f26d353ef6b131404e6cc4919741d80699a633be
Molly S. Peeples <molly@stsci.edu>
2018-02-02 15:15:34 -0500

    get_refine_box now in separate file

--- modular_plots.py
+++ modular_plots.py
@@ -15,6 +15,7 @@ import cPickle
 from astropy.table import Table
 
 from consistency import *
+from get_refine_box import get_refine_box
 from get_halo_center import get_halo_center
 from get_proper_box_size import get_proper_box_size
 
@@ -409,24 +410,6 @@ def make_si3_plots(ds, prefix, **kwargs):
         p.annotate_scale(size_bar_args={'color':'white'})
         p.hide_axes()
         p.save(prefix + 'ions/' + ds.basename + '_SiIII' + appendix)
-
-def get_refine_box(ds, zsnap, track):
-    ## find closest output, modulo not updating before printout
-    diff = track['col1'] - zsnap
-    this_loc = track[np.where(diff == np.min(diff[np.where(diff > 1.e-6)]))]
-    print("using this loc:", this_loc)
-    x_left = this_loc['col2'][0]
-    y_left = this_loc['col3'][0]
-    z_left = this_loc['col4'][0]
-    x_right = this_loc['col5'][0]
-    y_right = this_loc['col6'][0]
-    z_right = this_loc['col7'][0]
-
-    refine_box_center = [0.5*(x_left+x_right), 0.5*(y_left+y_right), 0.5*(z_left+z_right)]
-    refine_box = ds.r[x_left:x_right, y_left:y_right, z_left:z_right]
-    refine_width = np.abs(x_right - x_left)
-
-    return refine_box, refine_box_center, refine_width
 
 #-----------------------------------------------------------------------------------------------------
 
