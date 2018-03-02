318b114e936dea7c0ae085c0b1241b6d014c4cb3
JT <tumlinson@stsci.edu>
2018-03-02 14:59:28 -0500

    test commit from Atom

--- trees.py
+++ trees.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 """
 Functions related to post-rockstar consistent trees.
+JT Feb 2018
 """
-from astropy.table import Table 
+from astropy.table import Table
 
-def import_tree(filename): 
+def import_tree(filename):
     """
-    Read in the consistent tree and return a convenient representation of it in as astropy table 
+    Read in the consistent tree and return a convenient representation of it in as astropy table
     """
 
     a = Table.read(filename, format='ascii.commented_header')
 
-    tree = Table([ a['scale(0)'], a['id(1)'], a['desc_scale(2)'], a['desc_id(3)'], 
-                   a['Mvir(10)'], a['Rvir(11)'], 
-                   a['x(17)'], a['y(18)'], a['z(19)'], 
-                   a['Tree_root_ID(29)'], a['Orig_halo_ID(30)'], a['Snap_idx(31)'] ], 
-                   names=['ascale', 'id','desc_scale','desc_id','mvir','rvir','x','y','z','rootID','origID','snapidx']) 
+    tree = Table([ a['scale(0)'], a['id(1)'], a['desc_scale(2)'], a['desc_id(3)'],
+                   a['Mvir(10)'], a['Rvir(11)'],
+                   a['x(17)'], a['y(18)'], a['z(19)'],
+                   a['Tree_root_ID(29)'], a['Orig_halo_ID(30)'], a['Snap_idx(31)'] ],
+                   names=['ascale', 'id','desc_scale','desc_id','mvir','rvir','x','y','z','rootID','origID','snapidx'])
 
-    return tree 
+    return tree
 
-def select_root(tree, rootindex): 
+def select_root(tree, rootindex):
     """
-    Select out the halos with a root of the given index and return the new, pared back tree. 
+    Select out the halos with a root of the given index and return the new, pared back tree.
     """
 
-    new = tree[tree['rootID'] == rootindex] 
- 
+    new = tree[tree['rootID'] == rootindex]
+
     return new 
