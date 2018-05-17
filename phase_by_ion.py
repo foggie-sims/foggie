62c4752417623426ee04d73d68afac29b9a53bd1
Jason Tumlinson <tumlinson@lefty.local>
2018-05-17 17:28:28 -0400

    phase_by_ion works

--- phase_by_ion.py
+++ phase_by_ion.py
@@ -3,6 +3,7 @@ from datashader.utils import export_image
 import datashader.transfer_functions as tf
 import pandas as pd
 import yt
+import trident 
 import numpy as np
 from astropy.table import Table
 from functools import partial
@@ -13,10 +14,9 @@ from get_refine_box import get_refine_box as grb
 # this will show phase diagrams with the category coding done by ion 
 # fraction instead of "temperature" or whatever 
 
-
 from consistency import *
 
-def plot(fname, trackfile, ion=['O VI']):
+def plot(fname, trackfile, ion_list=['O VI'], radius=500.):
 
     background = 'white'
     plot_width  = int(1080)
@@ -25,18 +25,21 @@ def plot(fname, trackfile, ion=['O VI']):
     export = partial(export_image, background = background, export_path="export")
     cm = partial(colormap_select, reverse=(background!="black"))
 
-    # preliminaries - get the snapshot, and its refine box, and then "all_data"
+    # preliminaries - get the snapshot, add ion field, obtain the refine box, and then "all_data"
     ds=yt.load(fname)
 
     trident.add_ion_fields(ds, ions=ion_list) # ask trident to add fields for the ions you want 
-    #lines = ['O_p5_number_density'] 
 
     track = Table.read(trackfile, format='ascii')
     track.sort('col1')
     refine_box, refine_box_center, refine_width = grb(ds, ds.current_redshift, track)
     print('Refine box : ', refine_box) 
     print('Refine box center: ', refine_box_center) 
-    ad = refine_box
+    #ad = refine_box
+  
+    #replace with sphere 
+    sph = ds.sphere(center=refine_box_center, radius=(500, 'kpc'))
+    ad = sph 
 
     cell_vol = ad["cell_volume"]
     cell_mass = ad["cell_volume"].in_units('kpc**3') * ad["density"].in_units('Msun / kpc**3')
@@ -51,57 +54,54 @@ def plot(fname, trackfile, ion=['O VI']):
     dens = np.log10(ad['density'])
     temp = np.log10(ad['temperature'])
     mass = np.log10(cell_mass) # log of the cell mass in Msun
+    f_o6 = ad['O_p5_ion_fraction'] 
 
-    if (False):
-        #categorize by metallicity'
-        metallicity = ad['PartType0', 'Metallicity']
-        metal_label = np.chararray(np.size(dens), 5)
-        metal_label[metallicity < 10.] = 'high'
-        metal_label[metallicity < 0.005] = 'solar'
-        metal_label[metallicity < 0.000001] = 'low'
-        metal_label[metallicity < 0.0000001] = 'poor'
-        metal_color_key = {'high':'yellow', 'solar':'green', 'low':'purple', 'poor':'salmon'}
-
-    #categorize by phase
-    phase = np.chararray(np.size(dens), 4)
-    phase[temp < 9.] = 'hot'
-    phase[temp < 6.] = 'warm'
-    phase[temp < 5.] = 'cool'
-    phase[temp < 4.] = 'cold'
+    #categorize by ion fraction 
+    frac = np.chararray(np.size(dens), 4)
+    frac[f_o6 > -10.] = 'all'
+    frac[f_o6 > 0.01] = 'low' # yellow 
+    frac[f_o6 > 0.1] = 'med'  # orange 
+    frac[f_o6 > 0.2] = 'high' # red 
     dens = (dens + 24.) / 6.
     temp = (temp - 6.5) / 3.
     mass = (mass - 3.0) / 5.
+    print(np.min(f_o6), np.max(f_o6)) 
+
+    frac_color_key = {b'all':'black',
+                      b'high':'red',
+                      b'med':'yellow',
+                      b'low':'green'}
 
     df = pd.DataFrame({'x':x_particles, 'y':y_particles, 'z':z_particles, \
-         'temp':temp, 'dens':dens, 'mass': mass, 'phase':phase})
-    df.phase = df.phase.astype('category')
-    #df.metal_label = df.metal_label.astype('category')
+         'temp':temp, 'dens':dens, 'mass': mass, 'frac':frac})
+    df.frac = df.frac.astype('category')
 
     galaxy = ((-1.1,1.1),(-1.0,1.0))
 
     def create_image(frame, x_range, y_range, w=plot_width, h=plot_height):
         cvs = dshader.Canvas(plot_width=w, plot_height=h, x_range=x_range, y_range=y_range)
-        agg = cvs.points(frame, 'x', 'y', dshader.count_cat('phase'))
-        img = tf.shade(agg, color_key=phase_color_key, how='eq_hist')
+        agg = cvs.points(frame, 'dens', 'temp', dshader.count_cat('frac'))
+        img = tf.shade(agg, color_key=frac_color_key, how='linear')
         return img
 
     def create_image2(frame, x_range, y_range, w=plot_width, h=plot_height):
         cvs = dshader.Canvas(plot_width=w, plot_height=h, x_range=x_range, y_range=y_range)
-        agg = cvs.points(frame, 'x', 'mass', dshader.count_cat('phase'))
-        raw = tf.shade(agg, color_key=phase_color_key, how='eq_hist')
-        img = tf.spread(raw, px=1, shape='square')
+        agg = cvs.points(frame, 'x', 'y', dshader.count_cat('frac'))
+        img = tf.shade(agg, color_key=frac_color_key, how='linear')
         return img
 
-    export(create_image2(df, *galaxy),"Phase_Alls0")
+    # all cells 
+    export(create_image(df, *galaxy),"O6frac_Phase_All")
+    export(create_image2(df, *galaxy),"O6frac_Proj_All")
 
-    frame = df[df['phase'] == b'hot']
-    export(create_image2(frame, *galaxy),"Phase_Hot")
+    frame = df[df['frac'] == b'high']
+    export(create_image(frame, *galaxy),"O6frac_Phase_High")
+    export(create_image2(frame, *galaxy),"O6frac_Proj_High")
 
-    frame = df[df['phase'] == b'warm']
-    export(create_image2(frame, *galaxy),"Phase_Warm")
+    frame = df[df['frac'] == b'med']
+    export(create_image(frame, *galaxy),"O6frac_Phase_Med")
+    export(create_image2(frame, *galaxy),"O6frac_Proj_Med")
 
-    frame = df[df['phase'] == b'cool']
-    export(create_image2(frame, *galaxy),"Phase_Cool")
-
-    frame = df[df['phase'] == b'cold']
-    export(create_image2(frame, *galaxy),"Phase_Cold")
+    frame = df[df['frac'] == b'low']
+    export(create_image(frame, *galaxy),"O6frac_Phase_Low")
+    export(create_image2(frame, *galaxy),"O6frac_Proj_Low")
