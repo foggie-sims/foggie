87f47e5b62860b33c215095d71e4cd2745b74780
Jason Tumlinson <tumlinson@lefty.local>
2018-05-18 16:03:40 -0400

    edits to phase_by_ion

--- phase_by_ion.py
+++ phase_by_ion.py
@@ -16,14 +16,14 @@ from get_refine_box import get_refine_box as grb
 
 from consistency import *
 
-def plot(fname, trackfile, ion_list=['O VI'], radius=500.):
+def plot(fname, trackfile, ion_list=['O VI'], radius=5000.):
 
     background = 'white'
     plot_width  = int(1080)
     plot_height = int(1080)
 
     export = partial(export_image, background = background, export_path="export")
-    cm = partial(colormap_select, reverse=(background!="black"))
+    cm = partial(colormap_select, reverse=(background != "black"))
 
     # preliminaries - get the snapshot, add ion field, obtain the refine box, and then "all_data"
     ds=yt.load(fname)
@@ -35,15 +35,16 @@ def plot(fname, trackfile, ion_list=['O VI'], radius=500.):
     refine_box, refine_box_center, refine_width = grb(ds, ds.current_redshift, track)
     print('Refine box : ', refine_box) 
     print('Refine box center: ', refine_box_center) 
-    #ad = refine_box
   
     #replace with sphere 
-    sph = ds.sphere(center=refine_box_center, radius=(500, 'kpc'))
+    print('Extracting sphere of radius: ', radius) 
+    sph = ds.sphere(center=refine_box_center, radius=(5000, 'kpc'))
     ad = sph 
 
     cell_vol = ad["cell_volume"]
     cell_mass = ad["cell_volume"].in_units('kpc**3') * ad["density"].in_units('Msun / kpc**3')
     cell_size = np.array(cell_vol)**(1./3.)
+    print(cell_vol) 
 
     x_particles = ad['x'].ndarray_view() + cell_size * (np.random.rand(np.size(cell_vol)) * 2. - 1. )
     y_particles = ad['y'].ndarray_view() + cell_size * (np.random.rand(np.size(cell_vol)) * 2. - 1. )
@@ -55,6 +56,9 @@ def plot(fname, trackfile, ion_list=['O VI'], radius=500.):
     temp = np.log10(ad['temperature'])
     mass = np.log10(cell_mass) # log of the cell mass in Msun
     f_o6 = ad['O_p5_ion_fraction'] 
+    print('X min max', np.min(x_particles), np.max(x_particles)) 
+    print('Y min max', np.min(y_particles), np.max(y_particles)) 
+    print('Z min max', np.min(z_particles), np.max(z_particles)) 
 
     #categorize by ion fraction 
     frac = np.chararray(np.size(dens), 4)
@@ -62,8 +66,8 @@ def plot(fname, trackfile, ion_list=['O VI'], radius=500.):
     frac[f_o6 > 0.01] = 'low' # yellow 
     frac[f_o6 > 0.1] = 'med'  # orange 
     frac[f_o6 > 0.2] = 'high' # red 
-    dens = (dens + 24.) / 6.
-    temp = (temp - 6.5) / 3.
+    dens = (dens + 25.) / 6.
+    temp = (temp - 5.0) / 3.
     mass = (mass - 3.0) / 5.
     print(np.min(f_o6), np.max(f_o6)) 
 
@@ -76,7 +80,8 @@ def plot(fname, trackfile, ion_list=['O VI'], radius=500.):
          'temp':temp, 'dens':dens, 'mass': mass, 'frac':frac})
     df.frac = df.frac.astype('category')
 
-    galaxy = ((-1.1,1.1),(-1.0,1.0))
+    phase  = ((-1.1,1.1),(-1.0,1.0))
+    galaxy = ((-3.1,3.1),(-3.1,3.1))
 
     def create_image(frame, x_range, y_range, w=plot_width, h=plot_height):
         cvs = dshader.Canvas(plot_width=w, plot_height=h, x_range=x_range, y_range=y_range)
@@ -91,17 +96,19 @@ def plot(fname, trackfile, ion_list=['O VI'], radius=500.):
         return img
 
     # all cells 
-    export(create_image(df, *galaxy),"O6frac_Phase_All")
-    export(create_image2(df, *galaxy),"O6frac_Proj_All")
+    export(create_image(df, *phase),fname[0:6]+"_O6frac_Phase_All")
+    export(create_image2(df, *galaxy),fname[0:6]+"_O6frac_Proj_All")
 
     frame = df[df['frac'] == b'high']
-    export(create_image(frame, *galaxy),"O6frac_Phase_High")
-    export(create_image2(frame, *galaxy),"O6frac_Proj_High")
+    export(create_image(frame, *phase),fname[0:6]+"_O6frac_Phase_High")
+    export(create_image2(frame, *galaxy),fname[0:6]+"_O6frac_Proj_High")
 
     frame = df[df['frac'] == b'med']
-    export(create_image(frame, *galaxy),"O6frac_Phase_Med")
-    export(create_image2(frame, *galaxy),"O6frac_Proj_Med")
+    export(create_image(frame, *phase),fname[0:6]+"_O6frac_Phase_Med")
+    export(create_image2(frame, *galaxy),fname[0:6]+"_O6frac_Proj_Med")
 
     frame = df[df['frac'] == b'low']
-    export(create_image(frame, *galaxy),"O6frac_Phase_Low")
-    export(create_image2(frame, *galaxy),"O6frac_Proj_Low")
+    export(create_image(frame, *phase),fname[0:6]+"_O6frac_Phase_Low")
+    export(create_image2(frame, *galaxy),fname[0:6]+"_O6frac_Proj_Low")
+
+
