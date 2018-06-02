b7768c2606b687c12594a383ece683b5b71b4b12
JT <tumlinson@stsci.edu>
2018-06-01 20:51:10 -0400

    further tweaks to shade_maps deprecated "Halo.py" which was a stub anyway.

--- shade_maps.py
+++ shade_maps.py
@@ -1,17 +1,21 @@
+
 """ a module for datashader renders of phase diagrams"""
 from functools import partial
 import datashader as dshader
 from datashader.utils import export_image
 import datashader.transfer_functions as tf
+import holoviews as hv
 import pandas as pd
+import matplotlib.cm as cm
 import yt
 import trident
 import numpy as np
 from astropy.table import Table
 from get_refine_box import get_refine_box as grb
 from consistency import ion_frac_color_key, phase_color_key, metal_color_key
-
-
+from holoviews.operation.datashader import datashade, aggregate
+from holoviews import Store
+hv.extension('matplotlib')
 
 def prep_dataset(fname, trackfile, ion_list=['H I'], region='trackbox'):
     """prepares the dataset for rendering by extracting box or sphere"""
@@ -38,7 +42,6 @@ def prep_dataset(fname, trackfile, ion_list=['H I'], region='trackbox'):
 
     return all_data, refine_box, refine_width
 
-
 def categorize_by_temp(temp):
     """ define the temp category strings"""
     phase = np.chararray(np.size(temp), 4)
@@ -47,7 +50,6 @@ def categorize_by_temp(temp):
     phase[temp < 5.] = 'cool'
     phase[temp < 4.] = 'cold'
     return phase
-
 
 def categorize_by_fraction(f_ion):
     """ define the ionization category strings"""
@@ -60,18 +62,17 @@ def categorize_by_fraction(f_ion):
 
 def categorize_by_metallicity(metallicity):
     """ define the metallicity category strings"""
-    metal_label = np.chararray(np.size(metal), 5)
+    metal_label = np.chararray(np.size(metallicity), 5)
     metal_label[metallicity < 10.] = 'high'
     metal_label[metallicity < 0.005] = 'solar'
     metal_label[metallicity < 0.000001] = 'low'
     metal_label[metallicity < 0.0000001] = 'poor'
-    return metal
+    return metal_label
 
 def scale_lvec(lvec):
     lvec[lvec > 0.] = (np.log10(lvec[lvec > 0.]) - 25.) / 8.
     lvec[lvec < 0.] = (-1. * np.log10(-1.*lvec[lvec < 0.]) + 25.) / 8.
     return lvec
-
 
 def prep_dataframe(all_data, refine_box, refine_width):
     """ add fields to the dataset, create dataframe for rendering"""
@@ -97,20 +98,25 @@ def prep_dataframe(all_data, refine_box, refine_width):
     lz = scale_lvec(all_data['specific_angular_momentum_z'])
 
     f_o6 = all_data['O_p5_ion_fraction']
+    logf_o6 = np.log10(all_data['O_p5_ion_fraction'] + 1e-9)
     f_c4 = all_data['C_p3_ion_fraction']
     f_si4 = all_data['Si_p3_ion_fraction']
     phase = categorize_by_temp(np.log10(temperature))
+    metal = categorize_by_metallicity(all_data['metallicity'])
 
-    dens = (np.log10(density) + 25.) / 6.
-    temp = (np.log10(all_data['temperature']) - 5.0) / 3.
-    mass = (np.log10(cell_mass) - 3.0) / 5.
+    dens = np.log10(density)      # (np.log10(density) + 25.) / 6.
+    temp = np.log10(temperature)  # (np.log10(all_data['temperature']) - 5.0) / 3.
+    mass = np.log10(cell_mass)    # (np.log10(cell_mass) - 3.0) / 5.
 
     data_frame = pd.DataFrame({'x':x_particles, 'y':y_particles, \
                                'z':z_particles, 'temp':temp, 'dens':dens, \
+                               'cell_volume':cell_vol, \
                                'mass': mass, \
                                'phase':phase, \
                                'lx':lx, 'ly':ly, 'lz':lz, \
                                'f_o6':f_o6, 'f_c4':f_c4, 'f_si4':f_si4, \
+                               'logf_o6':logf_o6, \
+                               'metal': metal, \
                                'o6frac':categorize_by_fraction(f_o6),\
                                'c4frac':categorize_by_fraction(f_c4),\
                                'si4frac':categorize_by_fraction(f_si4)})
@@ -118,9 +124,9 @@ def prep_dataframe(all_data, refine_box, refine_width):
     data_frame.c4frac = data_frame.c4frac.astype('category')
     data_frame.si4frac = data_frame.si4frac.astype('category')
     data_frame.phase = data_frame.phase.astype('category')
+    data_frame.metal = data_frame.metal.astype('category')
 
     return data_frame
-
 
 def render_image(frame, field1, field2, count_cat, x_range, y_range, filename):
     """ renders density and temperature 'Phase' with linear aggregation"""
@@ -134,12 +140,25 @@ def render_image(frame, field1, field2, count_cat, x_range, y_range, filename):
         color_key = ion_frac_color_key
     elif 'phase' in count_cat:
         color_key = phase_color_key
+    elif 'metal' in count_cat:
+        color_key = metal_color_key
 
-    img = tf.shade(agg, color_key=color_key, how='linear')
+    img = tf.shade(agg, color_key=color_key, how='eq_hist')
     export(img, filename)
 
+def holoviews(frame):
+    """ for JT to learn DS stuff with axes - based on LC code"""
 
-
+    phase = hv.Scatter(frame,kdims=['dens'],vdims=['temp'],label="phase")
+    phase_shade = aggregate(hv.Scatter(frame, ['dens', 'temp']),
+                            y_range=(-1, 1), x_range=(-1,1),
+                            aggregator=dshader.count_cat('phase'))
+    phase_shade = phase_shade.opts(plot=dict(colorbar=False, aspect='square',
+                            logz=True, height=1080, width=1080),
+                            style=dict(cmap=cm.Reds, alpha=0.6))
+    renderer = Store.renderers['matplotlib'].instance(fig='pdf', holomap='gif')
+    renderer.save(phase_shade, "phase_red")
+    return
 
 def drive(fname, trackfile, ion_list=['H I', 'C IV', 'Si IV', 'O VI']):
     """this function drives datashaded phase plots"""
@@ -149,24 +168,22 @@ def drive(fname, trackfile, ion_list=['H I', 'C IV', 'Si IV', 'O VI']):
 
     data_frame = prep_dataframe(all_data, refine_box, refine_width)
 
-    phase = ((-1.1, 1.1), (-1.1, 1.1))
-    proj = ((-3.1, 3.1), (-3.1, 3.1))
+    holoviews(data_frame)
 
     for ion in ['o6', 'c4', 'si4']:
-        render_image(data_frame, 'dens', 'temp', ion+'frac', *phase,
-                     'RD0020_phase_'+ion)
-        render_image(data_frame, 'x', 'y', ion+'frac', *proj,
-                     'RD0020_proj_'+ion)
+        render_image(data_frame, 'dens', 'temp', ion+'frac',
+                     (-31, -20), (2,8), 'RD0020_phase_'+ion)
+        render_image(data_frame, 'x', 'y', ion+'frac',
+                     (-3,3), (-3,3), 'RD0020_proj_'+ion)
 
-    render_image(data_frame, 'temp', 'f_o6', 'phase', *phase, 'RD0020_ionfrac')
-    render_image(data_frame, 'dens', 'temp', 'phase', *phase, 'RD0020_phase')
-    render_image(data_frame, 'x', 'y', 'phase', *proj, 'RD0020_proj')
-    render_image(data_frame, 'x', 'mass', 'phase', *proj, 'RD0020_mass')
-    render_image(data_frame, 'x', 'lz', 'phase', *phase, 'RD0020_lz')
+    render_image(data_frame, 'temp', 'logf_o6', 'phase', (2,8), (-5,0), 'RD0020_ionfrac')
+    render_image(data_frame, 'dens', 'temp', 'phase', (-31,-20), (2,8), 'RD0020_phase')
+    render_image(data_frame, 'x', 'y', 'phase', (-3,3), (-3,3), 'RD0020_proj')
+    #render_image(data_frame, 'x', 'mass', 'phase', (-3.1, 3.1), (-3.1, 3.1), 'RD0020_mass')
+    #erender_image(data_frame, 'x', 'lz', 'phase', (-1.1, 1.1), (-1.1, 1.1), 'RD0020_lz')
 
-
-
-
+def howto():
+    """illustrates how to use this module for datashaded FOGGIE plots"""
 
 def cart2pol(x, y):
     return np.sqrt(x**2 + y**2), np.arctan2(y, x)
