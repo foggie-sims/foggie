d7edf63d77519438bd1d7d43f1ff471246e6c06a
Molly Peeples <molly@stsci.edu>
2018-06-06 13:32:19 -0400

    makes a table of info from the chosen misty fits file headers in a directory

---
+++ scrape_misty_headers.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+from astropy.io import fits
+from astropy.table import Table
+from astropy.io import ascii
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import os
+import glob
+
+def scrape_misty_headers():
+    DATA_DIR = "."
+    dataset_list = glob.glob(os.path.join(DATA_DIR, 'hlsp*v5_lsf.fits.gz'))
+    print(len(dataset_list))
+
+    line_list = ['H I 1216', 'H I 919', \
+    #                  'Al II 1671', 'Al III 1855', \
+                     'Si II 1260', 'Si III 1207', 'Si IV 1394', \
+                     'C II 1335', 'C III 977', 'C IV 1548', \
+                     'O VI 1032', 'Ne VIII 770']
+    ### 'H I 1026', 'H I 973', 'H I 950' probably also exist but ignore for now
+
+    data = Table(names=("impact","HI_col","HI_1216_Nmin","HI_1216_Ncomp","HI_919_Nmin","HI_919_Ncomp",\
+                    'Si_II_col','Si_II_Nmin','Si_II_Ncomp','Si_II_EW','Si_II_dv90',\
+                    'Si_III_col','Si_III_Nmin','Si_III_Ncomp','Si_III_EW','Si_III_dv90',\
+                    'Si_IV_col','Si_IV_Nmin','Si_IV_Ncomp','Si_IV_EW','Si_IV_dv90',\
+                    'C_II_col','C_II_Nmin','C_II_Ncomp','C_II_EW','C_II_dv90',\
+                    'C_III_col','C_III_Nmin','C_III_Ncomp','C_III_EW','C_III_dv90',\
+                    'C_IV_col','C_IV_Nmin','C_IV_Ncomp','C_IV_EW','C_IV_dv90',\
+                    'O_VI_col','O_VI_Nmin',"O_VI_Ncomp","O_VI_EW",'O_VI_dv90'), \
+             dtype=("f8","f8","f8","f8","f8","f8",
+                    "f8","f8",'f8','f8','f8',  # Si II
+                    'f8','f8','f8','f8','f8',  # Si III
+                    'f8','f8','f8','f8','f8',  # Si IV
+                    "f8","f8",'f8','f8','f8',  # C II
+                    'f8','f8','f8','f8','f8',  # C III
+                    'f8','f8','f8','f8','f8',  # C IV
+                    'f8',"f8",'f8','f8',"f8")) # O VI
+
+    for filename in dataset_list:
+        with fits.open(filename) as f:
+            row = [f[0].header['impact'], f['H I 1216'].header["tot_column"], f['H I 1216'].header["Nmin"]]
+            if 'Ncomp' in f['H I 1216'].header:
+                row = np.append(row, [f['H I 1216'].header['Ncomp']], axis=0)
+            else:
+                row = np.append(row, [-1], axis=0)
+            row = np.append(row, [f['H I 919'].header["Nmin"]], axis=0)
+            if 'Ncomp' in f['H I 919'].header:
+                row = np.append(row, [f['H I 919'].header['Ncomp']], axis=0)
+            else:
+                row = np.append(row, [-1], axis=0)
+            for ion in ['Si II 1260', 'Si III 1207', 'Si IV 1394','C II 1335', 'C III 977', 'C IV 1548', 'O VI 1032']:
+                if any([x.name.upper() == ion.upper() for x in f]):
+                    row = np.append(row, [f[ion].header["tot_column"], f[ion].header["Nmin"]], axis=0)
+                    if 'Ncomp' in f[ion].header:
+                        row = np.append(row, [f[ion].header['Ncomp']], axis=0)
+                    else:
+                        row = np.append(row, [-1], axis=0)
+                    if 'regEW0' in f[ion].header:
+                        row = np.append(row, [f[ion].header['regEW0']], axis=0)
+                    else:
+                        row = np.append(row, [-1], axis=0)
+                    if 'regdv900' in f[ion].header:
+                        row = np.append(row, [f[ion].header['regdv900']], axis=0)
+                    else:
+                        row = np.append(row, [-1], axis=0)
+                else:
+                    row = np.append(row, [-1], axis=0)
+                    row = np.append(row, [-1], axis=0)
+                    row = np.append(row, [-1], axis=0)
+                    row = np.append(row, [-1], axis=0)
+                    row = np.append(row, [-1], axis=0)
+            data.add_row(row)
+
+    # now let's save the table!
+    ascii.write(data, 'misty_v5_lsf.dat', format='fixed_width', overwrite=True)
+
+
+if __name__ == "__main__":
+    scrape_misty_headers()
