55a3ff4eb00b6beb3034aa6e2452957212ebfe79
Molly Peeples <molly@stsci.edu>
2018-07-10 11:02:35 -0400

    scrapes misty headers and makes nice tables

--- scrape_misty_headers.py
+++ scrape_misty_headers.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import numpy as np
 
 from astropy.io import fits
@@ -12,8 +14,8 @@ import glob
 
 def scrape_misty_headers():
     DATA_DIR = "."
-    dataset_list = glob.glob(os.path.join(DATA_DIR, 'hlsp*v5_lsf.fits.gz'))
-    print(len(dataset_list))
+    dataset_list = glob.glob(os.path.join(DATA_DIR, 'hlsp*rd0020*v5_rsp.fits.gz'))
+    print('there are',len(dataset_list),'fits files')
 
     line_list = ['H I 1216', 'H I 919', \
     #                  'Al II 1671', 'Al III 1855', \
@@ -22,7 +24,7 @@ def scrape_misty_headers():
                      'O VI 1032', 'Ne VIII 770']
     ### 'H I 1026', 'H I 973', 'H I 950' probably also exist but ignore for now
 
-    all_data = Table(names=("impact","HI_col","HI_1216_Nmin","HI_1216_Ncomp","HI_919_Nmin","HI_919_Ncomp",\
+    all_data = Table(names=("impact","HI_col","HI_1216_Nmin","HI_1216_EW","HI_1216_Ncomp","HI_919_Nmin","HI_919_Ncomp",\
                     'Si_II_col','Si_II_Nmin','Si_II_Ncomp','Si_II_EW','Si_II_dv90',\
                     'Si_III_col','Si_III_Nmin','Si_III_Ncomp','Si_III_EW','Si_III_dv90',\
                     'Si_IV_col','Si_IV_Nmin','Si_IV_Ncomp','Si_IV_EW','Si_IV_dv90',\
@@ -30,7 +32,7 @@ def scrape_misty_headers():
                     'C_III_col','C_III_Nmin','C_III_Ncomp','C_III_EW','C_III_dv90',\
                     'C_IV_col','C_IV_Nmin','C_IV_Ncomp','C_IV_EW','C_IV_dv90',\
                     'O_VI_col','O_VI_Nmin',"O_VI_Ncomp","O_VI_EW",'O_VI_dv90'), \
-             dtype=("f8","f8","f8","f8","f8","f8",
+             dtype=("f8","f8","f8","f8","f8","f8","f8",
                     "f8","f8",'f8','f8','f8',  # Si II
                     'f8','f8','f8','f8','f8',  # Si III
                     'f8','f8','f8','f8','f8',  # Si IV
@@ -59,6 +61,10 @@ def scrape_misty_headers():
     for filename in dataset_list:
         with fits.open(filename) as f:
             row = [f[0].header['impact'], f['H I 1216'].header["tot_column"], f['H I 1216'].header["Nmin"]]
+            if 'totEW' in f['H I 1216'].header:
+                row = np.append(row, [f['H I 1216'].header["totEW"]], axis=0)
+            else:
+                row = np.append(row, [-1], axis=0)
             if 'Ncomp' in f['H I 1216'].header:
                 row = np.append(row, [f['H I 1216'].header['Ncomp']], axis=0)
             else:
@@ -71,6 +77,7 @@ def scrape_misty_headers():
             for ion in ['Si II 1260', 'Si III 1207', 'Si IV 1394','C II 1335', 'C III 977', 'C IV 1548', 'O VI 1032']:
                 if any([x.name.upper() == ion.upper() for x in f]):
                     row = np.append(row, [f[ion].header["tot_column"], f[ion].header["Nmin"]], axis=0)
+                    #print(f[ion].header)
                     if 'Ncomp' in f[ion].header:
                         row = np.append(row, [f[ion].header['Ncomp']], axis=0)
                         if ion in ion_table_name_dict.keys():
@@ -92,6 +99,8 @@ def scrape_misty_headers():
                         row = np.append(row, [-1], axis=0)
                     if 'regdv900' in f[ion].header:
                         row = np.append(row, [f[ion].header['regdv900']], axis=0)
+                        #if 'regdv901' in f[ion].header:
+                        #    print(ion,"dv90 is", f[ion].header['regdv900'], "but also", f[ion].header['regdv901'] )
                     else:
                         row = np.append(row, [-1], axis=0)
                 else:
@@ -104,11 +113,11 @@ def scrape_misty_headers():
             i_file = i_file + 1
 
     # now let's save the table!
-    ascii.write(all_data, 'misty_v5_lsf.dat', format='fixed_width', overwrite=True)
-    ascii.write(si2_component_data, 'misty_si2_v5_lsf.dat', format='fixed_width', overwrite=True)
-    ascii.write(si4_component_data, 'misty_si4_v5_lsf.dat', format='fixed_width', overwrite=True)
-    ascii.write(c4_component_data, 'misty_c4_v5_lsf.dat', format='fixed_width', overwrite=True)
-    ascii.write(o6_component_data, 'misty_o6_v5_lsf.dat', format='fixed_width', overwrite=True)
+    ascii.write(all_data, 'misty_rd0020_v5_rsp.dat', format='fixed_width', overwrite=True)
+    ascii.write(si2_component_data, 'misty_rd0020_si2_v5_rsp.dat', format='fixed_width', overwrite=True)
+    ascii.write(si4_component_data, 'misty_rd0020_si4_v5_rsp.dat', format='fixed_width', overwrite=True)
+    ascii.write(c4_component_data, 'misty_rd0020_c4_v5_rsp.dat', format='fixed_width', overwrite=True)
+    ascii.write(o6_component_data, 'misty_rd0020_o6_v5_rsp.dat', format='fixed_width', overwrite=True)
 
 
 if __name__ == "__main__":
