745a4794075a2eefd2c406776ac52f33cd1df805
Jason Tumlinson <tumlinson@townes.local>
2018-08-21 08:02:45 -0400

    get_halo_center now returns physical units if asked

--- get_halo_center.py
+++ get_halo_center.py
@@ -10,28 +10,42 @@ def get_halo_center(ds, center_guess, **kwargs):
     Outputs center and velocity tuples.
     """
     # returns a list of the halo center coordinates
-    radius = kwargs.get("radius", 50.)  # search radius in kpc
+    radius = kwargs.get('radius', 50.)  # search radius in kpc
     vel_radius = kwargs.get('vel_radius', 2.)
+    units = kwargs.get('units', 'code')
 
+    length = 'code_length'
+    vel = 'code_velocity'
+
+    print('GET HALO CENTER UNITS', length, vel)
     # extract a sphere centered at the guess
-
     sphere_region = ds.sphere(center_guess, (radius, 'kpc'))
 
-    x_pos, y_pos, z_pos = np.array(sphere_region["x"]), \
-                          np.array(sphere_region["y"]), \
-                          np.array(sphere_region["z"])
+    x_pos, y_pos, z_pos = np.array(sphere_region["x"].in_units(length)), \
+                          np.array(sphere_region["y"].in_units(length)), \
+                          np.array(sphere_region["z"].in_units(length))
 
     dm_density = sphere_region['Dark_Matter_Density']
-
     # now determine the location of the highest DM density, which should be the
     # center of the main halo
     imax = (np.where(dm_density > 0.9999 * np.max(dm_density)))[0]
     halo_center = [x_pos[imax[0]], y_pos[imax[0]], z_pos[imax[0]]]
-    print('Located the main halo at:', halo_center)
 
-    sph = ds.sphere(halo_center, (vel_radius, 'kpc'))
+    sph = ds.sphere(halo_center, (5., 'kpc'))
     velocity = [np.mean(sph['x-velocity']),
                 np.mean(sph['y-velocity']),
                 np.mean(sph['z-velocity'])]
+
+    if (units == 'physical'): # do it over again but in the physical units
+        x_pos, y_pos, z_pos = np.array(sphere_region["x"].in_units('kpc')), \
+                              np.array(sphere_region["y"].in_units('kpc')), \
+                              np.array(sphere_region["z"].in_units('kpc'))
+        halo_center = [x_pos[imax[0]], y_pos[imax[0]], z_pos[imax[0]]]
+        velocity = [np.mean(sph['x-velocity'].in_units('km/s')),
+                        np.mean(sph['y-velocity'].in_units('km/s')),
+                        np.mean(sph['z-velocity'].in_units('km/s'))]
+
+    print('Located the main halo at:', halo_center, velocity)
+
 
     return halo_center, velocity
