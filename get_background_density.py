e264c6aab4191c17b9250f8ad92c9c6533739289
Jason Tumlinson <tumlinson@townes.local.stsci.edu>
2018-08-27 09:42:27 -0400

    units fix for get_background_density

--- get_background_density.py
+++ get_background_density.py
@@ -2,13 +2,15 @@
 
 import yt
 import numpy as np
-
+import os 
+os.sys.path.insert(0, os.environ['FOGGIE_REPO'])
 from get_proper_box_size import get_proper_box_size
+from yt.units.yt_array import YTQuantity
 
 def get_background_density(ds):
 
     ad = ds.all_data()
-    proper_box_size = get_proper_box_size(ds)
+    proper_box_size = YTQuantity(get_proper_box_size(ds)/1000., 'Mpc')
     baryon_mass, particle_mass = ad.quantities.total_quantity(["cell_mass", "particle_mass"])
     BoxTotalMass = (baryon_mass + particle_mass).in_units('Msun')
     BoxDensity = BoxTotalMass / (proper_box_size)**3
--- get_halo_profile.py
+++ get_halo_profile.py
@@ -1,25 +1,33 @@
 
 import yt
-import numpy as np 
-import get_background_density 
+import numpy as np
+import os
+os.sys.path.insert(0, os.environ['FOGGIE_REPO'])
+import get_background_density
 
-def get_halo_profile(ds, halo_center): 
- 
-    BoxDensity = get_background_density.get_background_density(ds) 
-    radii = ds.arr((np.arange(100) * 5. + 50.), 'kpc').in_units('Mpc') 
+def get_halo_profile(ds, halo_center):
 
-    for radius_to_analyze in radii: 
-        sphere = ds.sphere(halo_center, radius_to_analyze) 
+    BoxDensity = get_background_density.get_background_density(ds)
+    radii = ds.arr((np.arange(100) * 3. + 10.), 'kpc').in_units('Mpc')
+
+    for radius_to_analyze in radii:
+        sphere = ds.sphere(halo_center, radius_to_analyze)
         baryon_mass, particle_mass = sphere.quantities.total_quantity(["cell_mass", "particle_mass"])
+        print("baryons:  ", baryon_mass)
+        print("particles:", particle_mass)
+
         TotalMass = (baryon_mass + particle_mass).in_units('Msun')
         GasMass = baryon_mass.in_units('Msun')
         ParticleMass = particle_mass.in_units('Msun')
         HaloDensity = TotalMass / (4. / 3. * np.pi * (radius_to_analyze)**3)
         ptype = sphere["particle_type"]
-        mass = sphere['particle_mass'].convert_to_units('Msun') 
+        mass = sphere['particle_mass'].convert_to_units('Msun')
         StarMass = np.sum(mass[ptype == 2])
-    
-        HaloOverDensity = HaloDensity/BoxDensity 
- 
-        if (HaloOverDensity < 200.): 
-	    return radius_to_analyze.in_units('kpc'), TotalMass, ParticleMass, GasMass, StarMass  
+
+        HaloOverDensity = HaloDensity/BoxDensity
+        print()
+        print("radius: ", radius_to_analyze, HaloOverDensity, TotalMass, GasMass, StarMass)
+        print()
+
+        if (HaloOverDensity < 200.):
+	           return radius_to_analyze.in_units('kpc'), TotalMass, ParticleMass, GasMass, StarMass
