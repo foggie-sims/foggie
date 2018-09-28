e75616e41e95310c1aa65c5a38be7d93e77df685
Molly Peeples <molly@stsci.edu>
2018-09-28 14:02:27 -0400

    phase keys are now ordered dicts

--- consistency.py
+++ consistency.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import seaborn as sns
+import collections
 import astropy.units as u
 from matplotlib.colors import to_hex
 import numpy as np
@@ -168,7 +169,7 @@ temp_colors = sns.blend_palette(
 phase_color_labels = [b'cold1', b'cold2', b'cold3', b'cool', b'cool1', b'cool2',
                       b'cool3', b'warm', b'warm1', b'warm2', b'warm3', b'hot',
                       b'hot1', b'hot2', b'hot3']
-new_phase_color_key = {}
+new_phase_color_key = collections.OrderedDict()
 for i in np.arange(np.size(phase_color_labels)):
     new_phase_color_key[phase_color_labels[i]] = to_hex(temp_colors[i])
 
@@ -197,35 +198,38 @@ metal_color_labels = [b'free', b'free1', b'free2', b'free3', b'poor',
                       b'low2', b'low3', b'solar', b'solar1', b'solar2',
                       b'solar3', b'high', b'high1', b'high2', b'high3', b'high4']
 
-metallicity_colors = sns.blend_palette(
-    ("black", "#4575b4", "#984ea3", "#984ea3", "#d73027",
+metallicity_colors = sns.blend_palette(("black", "#4575b4", "#984ea3", "#984ea3", "#d73027",
      "darkorange", "#ffe34d"), n_colors=21)
-new_metals_color_key = {b'free': to_hex(metallicity_colors[0]),
-                        b'free1': to_hex(metallicity_colors[1]),
-                        b'free2': to_hex(metallicity_colors[2]),
-                        b'free3': to_hex(metallicity_colors[3]),
-                        # purple
-                        b'poor': to_hex(metallicity_colors[4]),
-                        b'poor1': to_hex(metallicity_colors[5]),
-                        b'poor2': to_hex(metallicity_colors[6]),
-                        b'poor3': to_hex(metallicity_colors[7]),
-                        b'low': to_hex(metallicity_colors[8]),  # blue
-                        b'low1': to_hex(metallicity_colors[9]),
-                        b'low2': to_hex(metallicity_colors[10]),
-                        b'low3': to_hex(metallicity_colors[11]),
-                        # green
-                        b'solar': to_hex(metallicity_colors[12]),
-                        b'solar1': to_hex(metallicity_colors[13]),
-                        b'solar2': to_hex(metallicity_colors[14]),
-                        b'solar3': to_hex(metallicity_colors[15]),
-                        # yellow
-                        b'high': to_hex(metallicity_colors[16]),
-                        b'high1': to_hex(metallicity_colors[17]),
-                        b'high2': to_hex(metallicity_colors[18]),
-                        b'high3': to_hex(metallicity_colors[19]),
-                        # orange
-                        b'high4': to_hex(metallicity_colors[20])
-                        }
+new_metals_color_key = collections.OrderedDict()
+for i in np.arange(np.size(metal_color_labels)):
+    new_metals_color_key[metal_color_labels[i]] = to_hex(metallicity_colors[i])
+
+# new_metals_color_key = {b'free': to_hex(metallicity_colors[0]),
+#                         b'free1': to_hex(metallicity_colors[1]),
+#                         b'free2': to_hex(metallicity_colors[2]),
+#                         b'free3': to_hex(metallicity_colors[3]),
+#                         # purple
+#                         b'poor': to_hex(metallicity_colors[4]),
+#                         b'poor1': to_hex(metallicity_colors[5]),
+#                         b'poor2': to_hex(metallicity_colors[6]),
+#                         b'poor3': to_hex(metallicity_colors[7]),
+#                         b'low': to_hex(metallicity_colors[8]),  # blue
+#                         b'low1': to_hex(metallicity_colors[9]),
+#                         b'low2': to_hex(metallicity_colors[10]),
+#                         b'low3': to_hex(metallicity_colors[11]),
+#                         # green
+#                         b'solar': to_hex(metallicity_colors[12]),
+#                         b'solar1': to_hex(metallicity_colors[13]),
+#                         b'solar2': to_hex(metallicity_colors[14]),
+#                         b'solar3': to_hex(metallicity_colors[15]),
+#                         # yellow
+#                         b'high': to_hex(metallicity_colors[16]),
+#                         b'high1': to_hex(metallicity_colors[17]),
+#                         b'high2': to_hex(metallicity_colors[18]),
+#                         b'high3': to_hex(metallicity_colors[19]),
+#                         # orange
+#                         b'high4': to_hex(metallicity_colors[20])
+#                         }
 
 metal_labels = new_metals_color_key.keys()
 
