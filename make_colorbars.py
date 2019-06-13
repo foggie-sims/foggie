0fdd558b325ea7e3393a6169258106dd1c058a53
Molly Peeples <molly@stsci.edu>
2019-06-13 09:52:57 -0400

    density, temperature, and metallicity added

--- make_colorbars.py
+++ make_colorbars.py
@@ -6,6 +6,42 @@ mpl.rcParams['font.family'] = 'serif'
 from paper_consistency import *
 
 
+fig = plt.figure(figsize=(2.1,12))
+ax = fig.add_axes([0.05, 0.01, 0.36, 0.98])
+norm = mpl.colors.Normalize(vmin=np.log10(density_slc_min), vmax=np.log10(density_slc_max))
+cb = mpl.colorbar.ColorbarBase(ax, cmap=density_color_map,
+                                norm=norm,
+                                orientation='vertical',
+                                extend='both')
+cb.ax.tick_params(labelsize=20.)
+cb.set_label(r'log density [M$_{\odot}$/pc$^{-3}$]',fontsize=32.)
+plt.savefig('density_slice_colorbar.png')
+plt.savefig('density_slice_colorbar.pdf')
+
+fig = plt.figure(figsize=(2.1,12))
+ax = fig.add_axes([0.05, 0.01, 0.36, 0.98])
+norm = mpl.colors.Normalize(vmin=np.log10(temperature_min), vmax=np.log10(temperature_max))
+cb = mpl.colorbar.ColorbarBase(ax, cmap=temperature_color_map,
+                                norm=norm,
+                                orientation='vertical',
+                                extend='both')
+cb.ax.tick_params(labelsize=20.)
+cb.set_label(r'log temperature [K]',fontsize=32.)
+plt.savefig('temperature_colorbar.png')
+plt.savefig('temperature_colorbar.pdf')
+
+fig = plt.figure(figsize=(2.1,12))
+ax = fig.add_axes([0.05, 0.01, 0.36, 0.98])
+norm = mpl.colors.Normalize(vmin=np.log10(metal_min), vmax=np.log10(metal_max))
+cb = mpl.colorbar.ColorbarBase(ax, cmap=metal_color_map,
+                                norm=norm,
+                                orientation='vertical',
+                                extend='both')
+cb.ax.tick_params(labelsize=20.)
+cb.set_label(r'log metallicity [Z$_{\odot}$]',fontsize=32.)
+plt.savefig('metallicity_colorbar.png')
+plt.savefig('metallicity_colorbar.pdf')
+
 fig = plt.figure(figsize=(1.5,4))
 ax = fig.add_axes([0.05, 0.025, 0.4, 0.95])
 norm = mpl.colors.Normalize(vmin=np.log10(h1_proj_min), vmax=np.log10(h1_proj_max))
@@ -85,19 +121,6 @@ cb.set_ticklabels(['11','12','13','14','15'])
 cb.set_label(r'log OVI column density [cm$^{-2}$]',fontsize=20.)
 plt.savefig('o6_colorbar_small.png')
 plt.savefig('o6_colorbar_small.pdf')
-
-fig = plt.figure(figsize=(1.5,4))
-ax = fig.add_axes([0.05, 0.025, 0.4, 0.95])
-norm = mpl.colors.Normalize(vmin=np.log10(h1_proj_min), vmax=np.log10(h1_proj_max))
-cb = mpl.colorbar.ColorbarBase(ax, cmap=h1_color_map,
-                                norm=norm,
-                                orientation='vertical',
-                                extend='both')
-cb.ax.tick_params(labelsize=16.)
-cb.set_label(r'log HI column density [cm$^{-2}$]',fontsize=20.)
-plt.savefig('h1_colorbar_small.png')
-plt.savefig('h1_colorbar_small.pdf')
-
 
 fig = plt.figure(figsize=(1.5,12))
 ax = fig.add_axes([0.05, 0.1, 0.42, 0.86])
