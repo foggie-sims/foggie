4e9cd2e10bc9aec0a3c988180526a1a778df3557
Molly Peeples <molly@stsci.edu>
2019-06-14 16:03:39 -0400

    update imports

--- modular_plots.py
+++ modular_plots.py
@@ -18,7 +18,7 @@ except ImportError:
 from astropy.table import Table
 
 from consistency import *
-from get_refine_box import get_refine_box
+from utils.get_refine_box import get_refine_box
 from get_halo_center import get_halo_center
 from utils.get_proper_box_size import get_proper_box_size
 from get_run_loc_etc import get_run_loc_etc
