c70b2aeb02a3e23b81b3cbc0e1869869f7fb23d4
Molly Peeples <molly@stsci.edu>
2019-09-17 08:54:53 -0400

    mostly works for finding halo_info for all the outputs in the pwd

--- get_halo_info.py
+++ get_halo_info.py
@@ -3,9 +3,11 @@ from astropy.table import Table
 from astropy.io import ascii
 from yt.analysis_modules.star_analysis.api import StarFormationRate
 
+from utils.get_refine_box import get_refine_box
 from get_halo_center import get_halo_center
 import numpy as np
-
+import glob
+import os
 from enzoGalaxyProps import find_rvirial
 
 import seaborn as sns
@@ -14,33 +16,47 @@ density_color_map = sns.blend_palette(
 density_proj_min = 5e-2  # msun / pc^2
 density_proj_max = 1e4
 
+def _stars(pfilter, data):
+    return data[(pfilter.filtered_type, "particle_type")] == 2
+
+#this gets dark matter particles in zoom region only
+def _darkmatter(pfilter, data):
+    return data[(pfilter.filtered_type, "particle_type")] == 4
+
 yt.add_particle_filter("stars",function=_stars, filtered_type='all',requires=["particle_type"])
 yt.add_particle_filter("darkmatter",function=_darkmatter, filtered_type='all',requires=["particle_type"])
 
 
 def loop_over_halos():
-    ### do this way at later times
-    snaplist = np.arange(earlysnap,latesnap+1)
-    print(snaplist)
+    '''
+    This will get the halo_info for ALL of the outputs in the pwd
+    '''
+    print('assuming trackname is halo_track in the pwd')
+    trackname = 'halo_track'
+    print("opening track: " + trackname)
+    track = Table.read(trackname, format='ascii')
+    track.sort('col1')
 
-    t = Table(
-        names=('redshift', 'name', 'xc', 'yc', 'zc',
-        'rvir', 'Mvir', 'Mstar', 'Mism', 'SFR'))
+    t = Table(dtype=('f8','S6', 'f8', 'f8', 'f8',
+                    'f8', 'f8', 'f8','f8', 'f8'),
+            names=('redshift', 'name', 'xc', 'yc', 'zc',
+                    'Rvir', 'Mvir', 'Mstar', 'Mism', 'SFR'))
 
-    center_guess = first_center
-    search_radius = 10. ### COMOVING KPC
+    outs = glob.glob(os.path.join('.', '?D0???/?D0???'))
+    for snap in outs:
+        print(snap)
+        ds = yt.load(snap)
+        ds.add_particle_filter('stars')
+        ds.add_particle_filter('darkmatter')
+        row = get_halo_info(ds, track)
+        t.add_row(row)
 
-    for isnap in snaplist:
-        if (isnap > 999): name = 'DD'+str(isnap)
-        if (isnap <= 999): name = 'DD0'+str(isnap)
-        if (isnap <= 99): name = 'DD00'+str(isnap)
-        if (isnap <= 9): name = 'DD000'+str(isnap)
-        print(name)
-        ds = yt.load(name+'/'+name)
-        row = get_halo_info(ds)
+    ascii.write(t,'halo_info', format='fixed_width', overwrite=True)
 
 def get_halo_info(ds, track):
-
+    '''
+    Given a dataset and the trackfile, finds the halo center, R200, M200, and galaxy masses and SFRs
+    '''
     zsnap = ds.get_parameter('CosmologyCurrentRedshift')
     comoving_box_size = ds.get_parameter('CosmologyComovingBoxSize')
     print('Comoving Box Size:', comoving_box_size)
@@ -54,29 +70,27 @@ def get_halo_info(ds, track):
     rvir = find_rvirial(refine_box, ds, center)
     # Mvir is mass within rvir
     vir_sphere = ds.sphere(center,rvir)
-    gal_dm_mass = gal_sphere[('darkmatter', 'particle_mass')].in_units('Msun')
-    gal_dm_total_mass = gal_dm_mass.sum().value[()]
+    vir_dm_mass = vir_sphere[('darkmatter', 'particle_mass')].in_units('Msun')
+    Mvir = vir_dm_mass.sum()
 
     ## define where the central galaxy is
     fgal = 0.07 ## fraction of the virial radius we are saying is the galaxy radius; totes made up
     gal_sphere = ds.sphere(center, fgal*rvir)
     # Mstar is sum of stellar mass; Mism is sum of gas mass; SFR is sum of SFR
     gal_stars_mass = gal_sphere[('stars', 'particle_mass')].in_units('Msun')
-    gal_stars_total_mass = gal_stars_mass.sum().value[()]
+    Mstar = gal_stars_mass.sum()
 
     gal_ism_mass = gal_sphere['cell_mass'].in_units('Msun')
-    gal_ism_total_mass = gal_ism_mass.sum().value[()]
+    Mism = gal_ism_mass.sum()
 
     sfr = StarFormationRate(ds, data_source=gal_sphere)
 
-
-    row = [zsnap, name,
-        center[0], center[1], center[2],
-        rvir, Mvir, Mstar, Mism, sfr.Msol_yr])
-
+    row = [zsnap, 'RD0020',
+            center[0], center[1], center[2],
+            rvir, Mvir, Mstar, Mism, sfr.Msol_yr[-1]]
     return row
 
 
 if __name__ == "__main__":
 
-    loop_over_halos(first_center, 699, 580, 0.002)
+    loop_over_halos()
