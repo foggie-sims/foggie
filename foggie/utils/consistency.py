3a71bc04f0c5134f2cfedb82bce782d2cf84f2c8
yhzheng <yz2505@columbia.edu>
2019-10-03 16:31:01 -0700

    add colormaps for radius, v+ and v-

--- foggie/utils/consistency.py
+++ foggie/utils/consistency.py
@@ -385,11 +385,129 @@ def categorize_by_hi(hi):
     phase[hi < hi_vals[0]] = b'free'
     return phase
 
-colormap_dict = {'phase':new_phase_color_key, 'metal':new_metals_color_key,
-    'h1':hi_color_key, 'density':density_color_map, 'O_p5_number_density':o6_color_map, 
-    'H_p0_number_density':h1_color_map, 'C_p1_number_density': c2_color_map, 
-    'C_p3_number_density':c4_color_map}
+############### Yong add these categories for mocky way ##### 
+### categorize halo gas by radius.
+radius_df_colname = 'cat_radius' # name of radius in dataframe
+radius_color_labels = [b'r0-20', b'r20-40', b'r40-60', b'r60-80',
+                       b'r80-100', b'r100-120', b'r120-140', b'r140-160']
+# this color has been reserved for FOGGIE I and II paper, so now using a different one. 
+# radius_colors = sns.blend_palette(('salmon', '#984ea3', '#4daf4a',
+#                                    '#ffe34d', 'darkorange'), n_colors=8)
+# radius_colors = sns.cubehelix_palette(8)
+# radius_colors = sns.blend_palette(('#691F5E', '#4FCEED', '#F76C1D', '#F71D45'), n_colors=8)
+radius_colors = sns.blend_palette(('#691F5E', '#4FCEED', '#F76C1D', '#DAD10C'), n_colors=8)
+radius_discrete_cmap = mpl.colors.ListedColormap(radius_colors)
+radius_color_key = collections.OrderedDict()
+for i, ilabel in enumerate(radius_color_labels):
+    radius_color_key[ilabel] = to_hex(radius_colors[i])
 
+def categorize_by_radius(radius):
+    """ define the radius category strings"""
+    cat_radius = np.chararray(np.size(radius), 8)
+    cat_radius[np.all([radius>=0, radius<20], axis=0)] = b'r0-20'
+    cat_radius[np.all([radius>=20, radius<40], axis=0)] = b'r20-40'
+    cat_radius[np.all([radius>=40, radius<60], axis=0)] = b'r40-60'
+    cat_radius[np.all([radius>=60, radius<80], axis=0)] = b'r60-80'
+    cat_radius[np.all([radius>=80, radius<100], axis=0)] = b'r80-100'
+    cat_radius[np.all([radius>=100, radius<120], axis=0)] = b'r100-120'
+    cat_radius[np.all([radius>=120, radius<140], axis=0)] = b'r120-140'
+    cat_radius[np.all([radius>=140, radius<160], axis=0)] = b'r140-160'
+    return cat_radius
+
+### categorize halo gas by velocity.
+velocity_df_colname = 'cat_velocity' # this is the name of velocity in dataframe
+velocity_color_labels = [b'<-100', b'[-100, -50]', b'[-50, 0]', 
+                         b'[0, 50]', b'[50, 100]', b'>100']
+#velocity_colors = sns.blend_palette(('salmon', "#984ea3", "#4daf4a",
+#                            "#ffe34d", 'darkorange'), n_colors=10)
+#velocity_colors = sns.color_palette("coolwarm", n_colors=5)
+#velocity_colors=sns.blend_palette(('#3C92F9','#5FEAF0', '#A8A49F',
+#                                '#FEB147', '#F95B3C'), n_colors=6)
+#velocity_colors=sns.blend_palette(('#3C92F9','#5FEAF0', '#FCA024', 
+#                                   '#FE7C47', '#F95B3C'), n_colors=6)
+velocity_colors=sns.blend_palette(('#C1BEB4', '#5FEAF0', '#3C92F9', 
+                                   '#F95B3C', '#FCA024', '#EFD96B'), n_colors=6)
+velocity_discrete_cmap = mpl.colors.ListedColormap(velocity_colors)
+velocity_color_key = collections.OrderedDict()
+for i, ilabel in enumerate(velocity_color_labels):
+    velocity_color_key[ilabel] = to_hex(velocity_colors[i])
+
+def categorize_by_velocity(velocity):
+    """ define the line of sight velocity category strings"""
+    vv = velocity
+    cat_velocity = np.chararray(np.size(vv), 11)
+    cat_velocity[vv<-100] = b'<-100'
+    cat_velocity[np.all([vv>=-100, vv<-50], axis=0)] = b'[-100, -50]'
+    cat_velocity[np.all([vv>=-50, vv<0], axis=0)] = b'[-50, 0]'
+    cat_velocity[np.all([vv>=0, vv<50], axis=0)] = b'[0, 50]'
+    cat_velocity[np.all([vv>=50, vv<100], axis=0)] = b'[50, 100]'
+    cat_velocity[vv>100] = b'>100'
+    return cat_velocity
+
+##################### let's split velocity into +v and -v ############## 
+### categorize halo gas by velocity.
+vel_pos_df_colname = 'cat_vel_pos' # this is the name of velocity in dataframe
+vel_pos_color_labels = [b'[0, 50]', b'[50, 100]', b'[100, 150]', 
+                        b'[150, 200]', b'>200']
+#vel_pos_colors = sns.blend_palette(('#C1BEB4', '#EF9F6B', '#EE7666', '#F96B28', 
+#                                    '#EE66B4', '#EE66B4', '#CA11DC'), 
+#                                     n_colors=len(vel_pos_color_labels))
+#vel_pos_colors = sns.blend_palette(('#C1BEB4', '#EF9F6B', '#FA8824', 
+#                                    '#EE66B4', '#C91CCC'), 
+#                                     n_colors=len(vel_pos_color_labels))
+cmap = mpl.pyplot.cm.PuRd
+vel_pos_colors = sns.blend_palette((cmap(0.25), cmap(0.4), cmap(0.55), cmap(0.7), cmap(0.9)), 
+                                   n_colors=len(vel_pos_color_labels))
+vel_pos_discrete_cmap = mpl.colors.ListedColormap(vel_pos_colors)
+vel_pos_color_key = collections.OrderedDict()
+for i, ilabel in enumerate(vel_pos_color_labels):
+    vel_pos_color_key[ilabel] = to_hex(vel_pos_colors[i])
+
+def categorize_by_vel_pos(velocity):
+    """ define the line of sight velocity category strings"""
+    vv = velocity
+    cat_vel_pos = np.chararray(np.size(vv), 11)
+    cat_vel_pos[np.all([vv>=0, vv<50], axis=0)] = b'[0, 50]'
+    cat_vel_pos[np.all([vv>=50, vv<100], axis=0)] = b'[50, 100]'
+    cat_vel_pos[np.all([vv>=100, vv<150], axis=0)] = b'[100, 150]'
+    cat_vel_pos[np.all([vv>=150, vv<200], axis=0)] = b'[150, 200]'
+    cat_vel_pos[vv>200] = b'>200'
+    return cat_vel_pos
+
+##################### let's split velocity into +v and -v ############## 
+### categorize halo gas by velocity.
+vel_neg_df_colname = 'cat_vel_neg' # this is the name of velocity in dataframe
+vel_neg_color_labels = [b'<-200', b'[-200, -150]', b'[-150, -100]', 
+                        b'[-100, -50]', b'[-50, 0]'] 
+cmap = mpl.pyplot.cm.YlGnBu_r
+vel_neg_colors = sns.blend_palette((cmap(0.25), cmap(0.4), cmap(0.55), cmap(0.7), cmap(0.9)),
+                                   n_colors=len(vel_neg_color_labels))
+vel_neg_discrete_cmap = mpl.colors.ListedColormap(vel_neg_colors)
+vel_neg_color_key = collections.OrderedDict()
+for i, ilabel in enumerate(vel_neg_color_labels):
+    vel_neg_color_key[ilabel] = to_hex(vel_neg_colors[i])
+
+def categorize_by_vel_neg(velocity):
+    """ define the line of sight velocity category strings"""
+    vv = velocity
+    cat_vel_neg = np.chararray(np.size(vv), 12)
+    cat_vel_neg[vv<-200] = b'<-200'
+    cat_vel_neg[np.all([vv>=-200, vv<-150], axis=0)] = b'[-200, -150]'
+    cat_vel_neg[np.all([vv>=-150, vv<-100], axis=0)] = b'[-150, -100]'
+    cat_vel_neg[np.all([vv>=-100, vv<-50], axis=0)] = b'[-100, -50]'
+    cat_vel_neg[np.all([vv>=-50, vv<0], axis=0)] = b'[-50, 0]'
+    return cat_vel_neg
+
+############################################################ 
+
+colormap_dict = {'phase':new_phase_color_key, \
+                 'metal':new_metals_color_key, \
+                 'h1':hi_color_key, \
+                 'density':density_color_map, \
+                 'O_p5_number_density':o6_color_map, \
+                 'H_p0_number_density':h1_color_map, \
+                 'C_p1_number_density': c2_color_map, \
+                 'C_p3_number_density':c4_color_map}
 
 proj_max_dict = {'density':1e-1, 'H_p0_number_density':h1_proj_max, 
                   'C_p1_number_density':c2_max, 'C_p2_number_density':c3_max, 
