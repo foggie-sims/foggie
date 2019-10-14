2793055b215fb607a4b9623d474cd862f0b38b2a
RaymondSimons <rsimons@pha.jhu.edu>
2019-10-14 08:27:00 -0700

    bulk create qsub jobs

--- foggie/satellites/make_qsubs/make_tracksats_qsubs.py
+++ foggie/satellites/make_qsubs/make_tracksats_qsubs.py
@@ -25,14 +25,15 @@ for halo in halos:
     
 
     sf = open('/nobackupp2/rcsimons/foggie/submit_scripts/tracks/submit_%s_%.4i_%.4i_tracksats.sh'%(halo, DDmin, DDmax), 'w+')
-    for DD in arange(DDmin, DDmax):
-        snapname = '%s_%.4i'%(halo, DD)
-        qsub_fname = 'track_%s_%.4i.qsub'%(halo, DD)        
+    splitn = 10
+    for dmn in arange(DDmin, DDmax+splitn, splitn):
+        snapname = '%s_%.4i'%(halo, dmn)
+        qsub_fname = 'track_%s_%.4i.qsub'%(halo, dmn)        
         qf = open('/nobackupp2/rcsimons/foggie/submit_scripts/tracks/%s'%qsub_fname, 'w+')
         
         qf.write('#PBS -S /bin/bash\n')
         qf.write('#PBS -l select=1:ncpus=20:model=ivy\n')
-        qf.write('#PBS -l walltime=0:30:00\n')
+        qf.write('#PBS -l walltime=4:00:00\n')
         qf.write('#PBS -q normal\n')
         qf.write('#PBS -N %s\n'%snapname)
         qf.write('#PBS -M rsimons@jhu.edu\n')
@@ -43,12 +44,12 @@ for halo in halos:
         qf.write('#PBS -W group_list=s1938\n\n\n\n')  
 
         qf.write('source /u/rcsimons/.bashrc\n')
-        qf.write('python /u/rcsimons/git/foggie/foggie/satellites/track_satellites.py \
-                 --halo %s --output DD%.4i --system pleiades > ./outfiles/%s_track_satellites.err > \
-                 ./outfiles/%s_track_satellites.out\n'%(halo, DD, snapname, snapname))
+        for DD in arange(dmn, dmn+splitn):
+                qf.write('python /u/rcsimons/git/foggie/foggie/satellites/track_satellites.py \
+                         --halo %s --output DD%.4i --system pleiades_raymond > ./outfiles/%s_track_satellites.err > \
+                         ./outfiles/%s_track_satellites.out\n'%(halo, DD, snapname, snapname))
 
-        qf.close()
-
+       	qf.close()	
 
         sf.write('qsub %s\n'%qsub_fname)
 
