cca264e202dd1593d880a829fae99cfe31b2cac7
Raymond Simons <rsimons@stsci.edu>
2019-10-31 17:12:50 -0400

    add v arrows to proj plots; use cylinders instead of rays

--- foggie/satellites/track_satellites.py
+++ foggie/satellites/track_satellites.py
@@ -17,6 +17,8 @@ from foggie.utils.get_refine_box import get_refine_box
 from foggie.utils.get_halo_center import get_halo_center
 from foggie.utils.get_proper_box_size import get_proper_box_size
 from foggie.utils import yt_fields
+from foggie.satellites.make_satellite_projections import make_projection_plots
+from yt.units import kpc
 
 
 def parse_args():
@@ -74,7 +76,7 @@ def run_tracker(args, anchor_ids, sat, temp_outdir, id_all, x_all, y_all, z_all)
     print ('tracking %s %s'%(args.halo, sat))
     gd_indices = []
     print ('\t', args.halo, sat, 'finding anchor stars..')
-    for a, anchor_id in enumerate(anchor_ids[:1000]):
+    for a, anchor_id in enumerate(anchor_ids):
       match = where(id_all == anchor_id)[0]
       if len(match) > 0: gd_indices.append(int(match))
 
@@ -126,7 +128,8 @@ if __name__ == '__main__':
     temp_outdir = cat_dir + '/sat_track_locations/temp'
 
 
-    if not os.path.isfile('%s/%s/%s_%s.npy'%(temp_outdir.replace('/temp', ''), args.halo, args.halo, args.output)):
+    #if not os.path.isfile('%s/%s/%s_%s.npy'%(temp_outdir.replace('/temp', ''), args.halo, args.halo, args.output)):
+    if True:
       if not os.path.isdir(cat_dir): os.system('mkdir ' + cat_dir) 
       if not os.path.isdir(fig_dir): os.system('mkdir ' + fig_dir) 
       if not os.path.isdir(cat_dir + '/sat_track_locations'): os.system('mkdir ' + cat_dir + '/sat_track_locations')
@@ -148,112 +151,122 @@ if __name__ == '__main__':
 
       refine_box, refine_box_center, x_width = get_refine_box(ds, zsnap, track)
 
-      load_particle_fields = ['particle_index',\
-                              'particle_position_x', 'particle_position_y', 'particle_position_z']
-      filter_particles(refine_box, load_particles = True, load_particle_types = ['stars'], load_particle_fields = load_particle_fields)
-      #filter_particles(refine_box)
-      print ('particles loaded')
+      small_sp = ds.sphere(refine_box_center, (20, 'kpc'))
+      refine_box_bulk_vel = small_sp.quantities.bulk_velocity()
+      all_data = ds.all_data()
 
+      if False:
+        load_particle_fields = ['particle_index',\
+                                'particle_position_x', 'particle_position_y', 'particle_position_z']      
 
-      # Need to pass in these arrays, can't pass in refine box
-      id_all = refine_box['stars', 'particle_index']
-      x_all = refine_box['stars', 'particle_position_x'].to('kpc')
-      y_all = refine_box['stars', 'particle_position_y'].to('kpc')
-      z_all = refine_box['stars', 'particle_position_z'].to('kpc')
+        filter_particles(all_data, load_particles = True, load_particle_types = ['stars'], load_particle_fields = load_particle_fields)
 
-      Parallel(n_jobs = -1)(delayed(run_tracker)(args, anchors[sat]['ids'], sat, temp_outdir, id_all, x_all, y_all, z_all) for sat in anchors.keys())
-      #for sat in anchors.keys():
-      #     run_tracker(args, anchors[sat]['ids'], sat, temp_outdir, id_all, x_all, y_all, z_all)
+        print ('particles loaded')
+        # Need to pass in these arrays, can't pass in refine box
+        id_all = all_data['stars', 'particle_index']
+        x_all  = all_data['stars', 'particle_position_x'].to('kpc')
+        y_all  = all_data['stars', 'particle_position_y'].to('kpc')
+        z_all  = all_data['stars', 'particle_position_z'].to('kpc')
+    
+        Parallel(n_jobs = -1)(delayed(run_tracker)(args, anchors[sat]['ids'], sat, temp_outdir, id_all, x_all, y_all, z_all) for sat in anchors.keys())
+
+      else: 
+        filter_particles(refine_box)
 
       #Collect outputs      
       output = {}
       annotate_others = []
+      all_start_arrows = []
+      all_end_arrows = []
 
       for sat in anchors.keys():
-
+        if sat == 'c': break
         temp = np.load(temp_outdir + '/' + args.halo + '_' + args.output + '_' + sat + '.npy')
         output[sat] = {}
 
-               
+        sp = ds.sphere(center = ds.arr(temp, 'kpc'), radius = 1*kpc)
+  
+        com = sp.quantities.center_of_mass(use_gas=False, use_particles=True, particle_type = 'stars').to('kpc')
 
-        if np.isnan(temp[0]):
+        com_x = float(com[0].value)
+        com_y = float(com[1].value)
+        com_z = float(com[2].value)
 
-            output[sat]['x'] = np.nan
-            output[sat]['y'] = np.nan
-            output[sat]['z'] = np.nan
-            output[sat]['vx'] = np.nan
-            output[sat]['vy'] = np.nan
-            output[sat]['vz'] = np.nan
+        output[sat]['x'] = round(com_x, 3)
+        output[sat]['y'] = round(com_y, 3)
+        output[sat]['z'] = round(com_z, 3)
 
-            output[sat]['RP_vel'] = np.nan
-            output[sat]['RP_dens'] = np.nan
+        stars_vx = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_x'), ('stars', 'particle_mass')).to('km/s')
+        stars_vy = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_y'), ('stars', 'particle_mass')).to('km/s')
+        stars_vz = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_z'), ('stars', 'particle_mass')).to('km/s')
 
-        else:
-            from yt.units import kpc
-            sp = ds.sphere(center = ds.arr(temp, 'kpc'), radius = 1*kpc)
-      
-            com = sp.quantities.center_of_mass(use_gas=False, use_particles=True, particle_type = 'stars').to('kpc')
+        output[sat]['vx'] = round(float(stars_vx.value), 3)
+        output[sat]['vy'] = round(float(stars_vy.value), 3)
+        output[sat]['vz'] = round(float(stars_vz.value), 3)
 
-            com_x = float(com[0].value)
-            com_y = float(com[1].value)
-            com_z = float(com[2].value)
+        start = ds.arr([com_x, com_y, com_z],  'kpc')
 
-            output[sat]['x'] = round(com_x, 3)
-            output[sat]['y'] = round(com_y, 3)
-            output[sat]['z'] = round(com_z, 3)
+        
+        vel = ds.arr([stars_vx, stars_vy, stars_vz], 'km/s')
+        vel_norm= vel / np.sqrt(sum(vel**2.))
+        
+        disk = ds.disk(center = start + 2.5 * vel_norm * kpc, normal = vel_norm, radius = 0.5*kpc, height = 2.5*kpc)
 
-            stars_vx = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_x'), ('stars', 'particle_mass')).to('km/s')
-            stars_vy = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_y'), ('stars', 'particle_mass')).to('km/s')
-            stars_vz = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_z'), ('stars', 'particle_mass')).to('km/s')
+        
+        disk.set_field_parameter('center', start)
+        disk.set_field_parameter('bulk_velocity', ds.arr([output[sat]['vx'], output[sat]['vy'],output[sat]['vz']], 'km/s'))
 
-            output[sat]['vx'] = round(float(stars_vx.value), 3)
-            output[sat]['vy'] = round(float(stars_vy.value), 3)
-            output[sat]['vz'] = round(float(stars_vz.value), 3)
-
-            start = ds.arr([com_x, com_y, com_z],  'kpc')
-
-            forward_arr = ds.arr([stars_vx, stars_vy, stars_vz], 'km/s')
-            forward_arr/=np.sqrt(sum(forward_arr**2.))
-            forward_arr*=ds.arr(5, 'kpc')
-            end = start + forward_arr
-
-            ray = ds.r[start:end]
-            ray.set_field_parameter('center', start)
-            ray.set_field_parameter('bulk_velocity', ds.arr([output[sat]['vx'], output[sat]['vy'],output[sat]['vz']], 'km/s'))
-            output[sat]['ray_den'] = ray['gas', 'density'].to('g/cm**3')
-            output[sat]['ray_vel'] = ray['gas', 'radial_velocity'].to('km/s')
-            output[sat]['ray_dist'] = ray['index', 'radius'].to('kpc')
-
-            # Make individual satellite projection plots
-
-            from foggie.satellites.make_satellite_projections import make_projection_plots
-            sat_center = ds.arr([output[sat]['x'], output[sat]['y'], output[sat]['z']], 'kpc')
-
-            if sat == '0': halo_center = sat_center.copy()
-
-            fig_width = 10 * kpc
-
-            make_projection_plots(refine_box.ds, sat_center, refine_box, fig_width, fig_dir, haloname, \
-                                fig_end = 'satellite_{}_{}'.format(args.output, sat), \
-                                do = ['gas', 'stars'], axes = ['x'],  annotate_center = True, annotate_others = [],\
-                                add_velocity = False)
-
-            #if np.isnan(output[sat]['x']): continue
-            #annotate_others.append(ds.arr([output[sat]['x'], output[sat]['y'], output[sat]['z']], 'kpc'))
-
-      #make_projection_plots(refine_box.ds, halo_center, refine_box, x_width, fig_dir, haloname,\
-      #                      fig_end = 'central_{}'.format(args.output),\
-      #                      do = ['gas', 'stars'], axes = ['x'],\
-      #                      annotate_center = True, annotate_others = annotate_others, is_central = True)
-
-      #make_projection_plots(refine_box.ds, refine_box_center, refine_box, x_width, fig_dir, haloname,\
-      #                      fig_end = 'box_center_{}'.format(args.output),\
-      #                      do = ['gas', 'stars'], axes = ['x'],\
-      #                      annotate_center = True, annotate_others = annotate_others, is_central = True)
+        profiles = yt.create_profile(disk, ("index", "cylindrical_z"), [("gas", "density"), ('gas', 'velocity_cylindrical_z')], weight_field = ('index', 'cell_volume')) 
+        output[sat]['ray_den'] = profiles.field_data[("gas", "density")].to('g/cm**3.')
+        output[sat]['ray_vel'] = profiles.field_data[('gas', 'velocity_cylindrical_z')].to('km/s')
+        output[sat]['ray_dist'] = profiles.x.to('kpc')
 
 
-      np.save('%s/%s/%s_%s.npy'%(temp_outdir.replace('/temp', ''), args.halo, args.halo, args.output), output)
+        point_sat = ds.point(start)
+        overlap = point_sat & refine_box
+        
+        output[sat]['in_refine_box'] = overlap.sum("cell_volume") > 0
+        print (output[sat]['in_refine_box'])
 
-      os.system('rm %s/%s_%s_*.npy'%(temp_outdir, args.halo, args.output))
+        fig_width = 10 * kpc
+        start_arrow = start
+        vel_fixed = vel - refine_box_bulk_vel
+        vel_fixed_norm = vel_fixed / ds.arr(200, 'km/s')#np.sqrt(sum(vel_fixed**2.))
+
+        end_arrow = start + vel_fixed_norm * ds.arr(1, 'kpc')
+
+        make_projection_plots(all_data.ds, start, all_data, fig_width, fig_dir, haloname, \
+                            fig_end = 'satellite_{}_{}'.format(args.output, sat), \
+                            do = ['stars'], axes = ['x'],  annotate_positions = [start],\
+                            add_arrow = True, start_arrow = [start_arrow], end_arrow = [end_arrow])
+
+        if output[sat]['in_refine_box']:
+          annotate_others.append(ds.arr([output[sat]['x'], output[sat]['y'], output[sat]['z']], 'kpc'))
+          all_start_arrows.append(start_arrow)
+          all_end_arrows.append(end_arrow)
+
+      #x_width = 10*kpc
+
+      make_projection_plots(refine_box.ds, ds.arr(refine_box_center, 'code_length').to('kpc'),\
+                            refine_box, x_width, fig_dir, haloname,\
+                            fig_end = 'box_center_{}'.format(args.output),\
+                            do = ['gas', 'stars'], axes = ['x'],\
+                            annotate_positions = annotate_others, is_central = False, add_arrow = True,\
+                            start_arrow = all_start_arrows, end_arrow = all_end_arrows)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
