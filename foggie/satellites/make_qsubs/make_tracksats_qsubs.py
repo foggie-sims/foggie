c342ac4cdde5e2199c59046430b216d1e60789bd
RaymondSimons <rsimons@pha.jhu.edu>
2019-11-07 08:51:33 -0800

    change walltime

--- foggie/satellites/make_qsubs/make_tracksats_qsubs.py
+++ foggie/satellites/make_qsubs/make_tracksats_qsubs.py
@@ -28,13 +28,13 @@ for halo in halos:
     sf = open('/nobackupp2/rcsimons/foggie/submit_scripts/tracks/submit_%s_%.4i_%.4i_tracksats.sh'%(halo, DDmin, DDmax), 'w+')
     splitn = 5
     for dmn in arange(DDmin, DDmax+splitn, splitn):
-        snapname = '%s_%.4i'%(halo, dmn)
-        qsub_fname = 'track_%s_%.4i.qsub'%(halo, dmn)        
+        snapname = '%s_%.4i_%.4i'%(halo, dmn, dmn+splitn)
+        qsub_fname = 'track_%s_%.4i_%.4i.qsub'%(halo, dmn, dmn+splitn)        
         qf = open('/nobackupp2/rcsimons/foggie/submit_scripts/tracks/%s'%qsub_fname, 'w+')
         
         qf.write('#PBS -S /bin/bash\n')
         qf.write('#PBS -l select=1:ncpus=20:model=ivy\n')
-        qf.write('#PBS -l walltime=4:00:00\n')
+        qf.write('#PBS -l walltime=6:00:00\n')
         qf.write('#PBS -q normal\n')
         qf.write('#PBS -N %s\n'%snapname)
         qf.write('#PBS -M rsimons@jhu.edu\n')
@@ -46,9 +46,10 @@ for halo in halos:
 
         qf.write('source /u/rcsimons/.bashrc\n')
         for DD in arange(dmn, dmn+splitn):
+                snapname_temp = '%s_%.4i'%(halo, DD)
                 qf.write('python /nobackupp2/rcsimons/git/foggie/foggie/satellites/track_satellites.py \
                          --halo %s --output DD%.4i --system pleiades_raymond > ./outfiles/%s_track_satellites.err > \
-                         ./outfiles/%s_track_satellites.out\n'%(halo, DD, snapname, snapname))
+                         ./outfiles/%s_track_satellites.out\n'%(halo, DD, snapname_temp, snapname_temp))
 
        	qf.close()	
 
