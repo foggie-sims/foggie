26418f59f9499e68decf46cb2bc2024c3eb86aa4
Molly Peeples <molly@stsci.edu>
2019-11-07 17:36:17 -0500

    comma

--- foggie/utils/consistency.py
+++ foggie/utils/consistency.py
@@ -121,13 +121,13 @@ halo_dict = {   2392  :  'Hurricane' ,
                 4123  :  'Wigshifter' ,
                 5016  :  'Squall' ,
                 5036  :  'Maelstrom' ,
-                8508  :  'Tempest'
-                002392  :  'Hurricane' ,
-                002878  :  'Cyclone' ,
-                004123  :  'Wigshifter' ,
-                005016  :  'Squall' ,
-                005036  :  'Maelstrom' ,
-                008508  :  'Tempest' }
+                8508  :  'Tempest',
+                '002392'  :  'Hurricane' ,
+                '002878'  :  'Cyclone' ,
+                '004123'  :  'Wigshifter' ,
+                '005016'  :  'Squall' ,
+                '005036'  :  'Maelstrom' ,
+                '008508'  :  'Tempest' }
 
 linelist_jt = ['H I 1216', 'H I 919', \
                 'Mg II 2796', 'Si II 1260', 'Si III 1206', 'Si IV 1394', \
@@ -142,6 +142,7 @@ linelist_long = ['H I 1216', 'H I 1026', 'H I 973',
                'Si II 1260', 'Si III 1206', 'Si IV 1394', \
                'C II 1335', 'C III 977', 'C IV 1548', \
                'O VI 1032', 'Ne VIII 770']
+linelist_short = ['H I 1216', 'Si II 1260', 'O VI 1032']
 
 cgm_temperature_min = 1.5e4  #<---- in some FOGGIE codes this will be used to set a min
 cgm_density_max = 2e-26
