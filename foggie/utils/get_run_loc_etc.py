711829560124cbeee9a7eb40c26ba8544d7feb11
ramonaaugustin <raugustin@stsci.edu>
2020-03-31 20:33:36 -0400

    added system "ramona_pleiades"

--- foggie/utils/get_run_loc_etc.py
+++ foggie/utils/get_run_loc_etc.py
@@ -54,6 +54,10 @@ def get_run_loc_etc(args):
         foggie_dir = "/Users/raugustin/WORK/SIMULATIONS/"
         output_path = "/Users/raugustin/WORK/Outputs/"
         code_path = "/Users/raugustin/foggie/foggie/"
+    elif args.system == "ramona_pleiades":
+        foggie_dir = "/nobackup/mpeeples/"
+        output_path = "/home5/ragust4/WORK/Outputs/"
+        code_path = "/home5/ragust4/foggie/foggie/"
 
     if not args.pwd:
         if args.run == "natural":
