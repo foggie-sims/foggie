6910a27cc34186e7061a5ac2977fc4a39b93aeed
tumlinson <tumlinson@townes.local>
2020-04-27 12:04:34 -0400

    update to Paper 5 scripts

--- foggie/paper_plots/paper5/cell_masses.py
+++ foggie/paper_plots/paper5/cell_masses.py
@@ -6,8 +6,20 @@ trackfile = '/Users/tumlinson/Dropbox/FOGGIE/foggie/foggie/halo_tracks/008508/nr
 # nref11c_nref9f z = 0.0 RD0042 
 dsname = '/Users/tumlinson/Dropbox/FOGGIE/snapshots/halo_008508/nref11c_nref9f/RD0042/RD0042'
 
-sm.simple_plot(dsname, trackfile, 'temperature', 'cell_mass', 'phase', ( (1,8), (-2,6) ), \
+df, image, ds = sm.simple_plot(dsname, trackfile, 'temperature', 'cell_mass', 'phase', ( (1,8), (-2,6) ), \
                 'RD0042_11c_9f_temperature_cell_mass_cgm_phase', region='cgm')
 
-sm.simple_plot(dsname, trackfile, 'temperature', 'cell_mass', 'phase', ( (1,8), (-2,6) ), \
+df, image, ds = sm.simple_plot(dsname, trackfile, 'temperature', 'cell_mass', 'phase', ( (1,8), (-2,6) ), \
                 'RD0042_11c_9f_temperature_cell_mass_cgm_phase_fOVI', region='cgm', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
+
+
+
+
+# nref11n_nref10f z = 0.0 RD0042 
+dsname = '/Users/tumlinson/Dropbox/FOGGIE/snapshots/halo_008508/nref11n_nref10f/RD0042/RD0042'
+
+df, image, ds = sm.simple_plot(dsname, trackfile, 'temperature', 'cell_mass', 'phase', ( (1,8), (-2,6) ), \
+                'RD0042_11n_10f_temperature_cell_mass_cgm_phase', region='cgm')
+
+df, image, ds = sm.simple_plot(dsname, trackfile, 'temperature', 'cell_mass', 'phase', ( (1,8), (-2,6) ), \
+                'RD0042_11n_10f_temperature_cell_mass_cgm_phase_fOVI', region='cgm', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
