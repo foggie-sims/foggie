9454ddd119723fcb87094494bf792752e19644c2
tumlinson <tumlinson@townes.local>
2020-07-11 09:53:48 -0400

    code for abs catalogs and FRB processing

---
+++ foggie/clouds/absorber_catalogs.py
@@ -0,0 +1,64 @@
+from astropy.table import Table
+import matplotlib.pyplot as plt
+import foggie.render.shade_maps as sm
+from foggie.utils.consistency import *
+import numpy as np 
+
+absorber_axis_limits = {'radius_corrected':(0,200), 'radial_velocity_corrected':(-500,500), 
+                        'b_effective':(0,50), 'vel_dispersion':(-10,100), 'vx_corrected':(-500,500), 
+                        'vy_corrected':(-500,500), 'vz_corrected':(-500,500), 'redshift':(2,0)}
+absorber_axis_labels = {'radius_corrected':'Radius [kpc]', 'b_effective':'effective b [km s$^{-1}$]', 
+                        'radial_velocity_corrected':'Radial Velocity [km s$^{-1}$]', 'redshift':'z'}
+
+
+def read_absorber_catalog(filename):
+    #read the table
+    ab = Table.read('corrected_absorber_catalog.csv')
+    ab['phase'] = categorize_by_temp(np.log10(ab['temperature']))
+
+    #assign colors to phases using FOGGIE utilities 
+    colors = []
+    for p in zip(ab['phase']):
+        colors.append( new_phase_color_key[str.encode(p[0])] ) 
+
+    ab['colors'] = colors   
+
+    # add labels for each component 
+    sightline_label = []
+    absorber_label = []
+    for i in ab['absorber_index']: 
+        sightline_label.append(i[0:4])
+        absorber_label.append(i[-1])
+    
+    ab['sightline_label'] = sightline_label
+    ab['absorber_label'] = absorber_label
+
+    ab['b_thermal'] = (1.38e-16*ab['temperature'] / 16. / 1.67e-24)**0.5 / 1e5 # thermal b-value in km/s
+    ab['b_nonthermal'] = ab['vel_dispersion'] / 2.
+    ab['b_effective'] = (ab['b_thermal']**2 + ab['b_nonthermal']**2)**0.5
+
+    return ab
+
+
+def select_absorber_temperature(ab,T): 
+    ab_t = ab[(ab['temperature'] > T[0]) & (ab['temperature'] < T[1])]
+    return ab_t
+
+def select_absorber_redshift(ab,z): 
+    ab_z = ab[(ab['col_dens'] > 11) & (ab['redshift'] > z-0.03) & (ab['redshift'] < z+0.03)]
+    return ab_z
+
+#make the plots 
+def make_absorber_plot(ab_z, var1, var2): 
+ 
+    zlabel = str(np.round(ab_z['redshift'][0]*10.) / 10.)
+    
+    plt.figure(figsize=(8,8)) 
+    for rr, rrv, rcolor in zip(ab_z[var1], ab_z[var2], ab_z['colors']): 
+        plt.plot(rr, rrv, 'o', markersize=8, color='#000000')
+        plt.plot(rr, rrv, 'o', markersize=5, color=rcolor)
+    plt.xlim(absorber_axis_limits[var1])   
+    plt.ylim(absorber_axis_limits[var2])
+    plt.xlabel(absorber_axis_labels[var1])
+    plt.ylabel(absorber_axis_labels[var2])
+    plt.savefig('abs_'+var1+'_'+var2+'_z_'+zlabel+'.png',transparent=True)
--- foggie/paper_plots/paper5/cooling_times.py
+++ foggie/paper_plots/paper5/cooling_times.py
@@ -3,25 +3,25 @@ import foggie.render.shade_maps as sm
 #This track is always valid for 8508 regardless of "run" or resolution - right? 
 trackfile = '/Users/tumlinson/Dropbox/FOGGIE/foggie/foggie/halo_tracks/008508/nref11n_selfshield_15/halo_track_200kpc_nref10' 
 
-# nref11c_nref9f z = 0.1 RD0040 
-dsname = '/Users/tumlinson/Dropbox/FOGGIE/snapshots/halo_008508/nref11c_nref9f/RD0040/RD0040'
+# nref11c_nref9f z = 0.0 RD0042 
+dsname = '/Users/tumlinson/Dropbox/FOGGIE/snapshots/halo_008508/nref11c_nref9f/RD0042/RD0042'
 
+sm.simple_plot(dsname, trackfile, 'radius_corrected', 'temperature', 'phase', ( (0,200), (1,8) ), \
+                'RD0042_11c_9f_radius_temperature_cgm_phase', region='cgm')
 
-
-sm.simple_plot(dsname, trackfile, 'radius', 'temperature', 'phase', ( (0,200), (1,8) ), \
-                'RD0040_11c_9f_radius_temperature_rvir_phase', region='rvir')
-
-sm.simple_plot(dsname, trackfile, 'radius', 'temperature', 'phase', ( (0,200), (1,8) ), \
-                'RD0040_11c_9f_radius_temperature_rvir_phase_fOVI', region='rvir', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
-
-sm.simple_plot(dsname, trackfile, 'radius', 'cooling_time', 'phase', ( (0,200), (4,13) ), \
-                'RD0040_11c_9f_radius_cooling_time_rvir_phase', region='rvir')
-
-sm.simple_plot(dsname, trackfile, 'radius', 'cooling_time', 'phase', ( (0,200), (4,13) ), \
-                'RD0040_11c_9f_radius_cooling_time_rvir_phase_fOVI', region='rvir', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
+sm.simple_plot(dsname, trackfile, 'radius_corrected', 'cooling_time', 'phase', ( (0,200), (4,13) ), \
+                'RD0042_11c_9f_radius_cooling_time_cgm_phase', region='cgm')
 
 sm.simple_plot(dsname, trackfile, 'temperature', 'cooling_time', 'phase', ( (1,8), (4,13) ), \
-                'RD0040_11c_9f_temperature_cooling_time_rvir_phase', region='rvir')
+                'RD0042_11c_9f_temperature_cooling_time_cgm_phase', region='cgm')
+
+
+
+sm.simple_plot(dsname, trackfile, 'radius_corrected', 'cooling_time', 'phase', ( (0,200), (4,13) ), \
+                'RD0042_11c_9f_radius_cooling_time_cgm_phase_fOVI', region='cgm', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
+
+sm.simple_plot(dsname, trackfile, 'radius_corrected', 'temperature', 'phase', ( (0,200), (1,8) ), \
+                'RD0042_11c_9f_radius_temperature_cgm_phase_fOVI', region='cgm', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
 
 sm.simple_plot(dsname, trackfile, 'temperature', 'cooling_time', 'phase', ( (1,8), (4,13) ), \
-                'RD0040_11c_9f_temperature_cooling_time_rvir_phase_fOVI', region='rvir', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
+                'RD0042_11c_9f_temperature_cooling_time_cgm_phase_fOVI', region='cgm', screenfield='O_p5_ion_fraction', screenrange=[0.1, 1.])
