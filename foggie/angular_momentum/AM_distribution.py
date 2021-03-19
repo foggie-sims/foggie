8ea47d3c9c76afda0903b3b8a065ce59178a8734
Raymond Simons <rsimons@saje.local>
2021-03-19 13:51:34 -0400

    production script, phi-theta space

--- foggie/angular_momentum/AM_distribution.py
+++ foggie/angular_momentum/AM_distribution.py
@@ -1,3 +1,4 @@
+import yt
 import foggie
 from foggie.utils.get_run_loc_etc import get_run_loc_etc
 from foggie.utils.get_refine_box import get_refine_box
@@ -45,7 +46,7 @@ def parse_args():
 
     parser.add_argument('--save_dir', metavar='save_dir', type=str, action='store',
                         help='directory to save products')
-    parser.set_defaults(save_dir="~")
+    parser.set_defaults(save_dir="/nobackupp2/rcsimons/foggie")
 
     parser.add_argument('--use_halo_c_v', dest='use_halo_c_v', action='store_true',
                         help='just use the pwd?, default is no')
@@ -56,30 +57,125 @@ def parse_args():
 
 
 
+def find_disk_Lhat(ds):
+
+    sp = ds.sphere(ds.halo_center_kpc, (3, 'kpc'))
+
+    bulk_vel = sp.quantities.bulk_velocity().to('km/s')
+    sp.set_field_parameter('bulk_velocity', bulk_vel)
+
+    results = {}
+    results['L'] = []
+
+    for i in ['x', 'y', 'z']:
+        L_gas     = sp.quantities.total_quantity(('gas', 'angular_momentum_%s'%i))
+
+        results['L'].append(float(L_gas.to('cm**2*g/s').value))
+
+    results['L'] = np.array(results['L'])
+
+    disk_Lhat = results['L']/np.sqrt(np.sum(results['L']**2.))
+
+
+    return disk_Lhat, bulk_vel
+
+
+
+def radial_profile(ds, bulk_vel, mass_types, args, save = True, sp_rad = (100., 'kpc'), rbins = np.arange(0, 100, 0.5)*kpc):
+    sp_sm = ds.sphere(ds.halo_center_kpc, sp_rad)
+    sp_sm.set_field_parameter('bulk_velocity', bulk_vel)
+
+    L_dic = {}
+
+
+    for (low_temp, high_temp, mtype, clr) in mass_types:
+        print (mtype)
+        if low_temp < 0.:
+            #particles
+            sp_use = sp_sm
+            rname = (mtype, 'particle_radius')
+            xname = (mtype, 'particle_angular_momentum_x')
+            yname = (mtype, 'particle_angular_momentum_y')
+            zname = (mtype, 'particle_angular_momentum_z')
+            mname = (mtype, 'particle_mass')
+        else:
+            #gas w/temperature cut
+            sp_use = sp_sm.cut_region(["(obj['temperature'] > {}) & (obj['temperature'] < {})".format(low_temp, high_temp)])
+
+            rname = ('index', 'radius')
+            xname = ('gas', 'angular_momentum_x')
+            yname = ('gas', 'angular_momentum_y')
+            zname = ('gas', 'angular_momentum_z')
+            mname = ('gas', 'cell_mass')
+
+        Li       = yt.create_profile(sp_use, [rname], fields=[xname, yname, zname, mname], 
+                                     weight_field=None, accumulation=False, override_bins = {rname:rbins})
+
+        L_dic[mtype] = {}
+        L_dic[mtype]['rprof'] = {}
+        L_dic[mtype]['adist'] = {}
+
+        L_dic[mtype]['rprof']['r']    = Li.x.to('kpc')
+        L_dic[mtype]['rprof']['Lx']   = Li[xname].to('g*cm**2/s')
+        L_dic[mtype]['rprof']['Ly']   = Li[yname].to('g*cm**2/s')
+        L_dic[mtype]['rprof']['Lz']   = Li[zname].to('g*cm**2/s')
+        L_dic[mtype]['rprof']['mass'] = Li[mname].to('Msun')
+
+        Lx    = sp_use[xname].to('g*cm**2/s')
+        Ly    = sp_use[yname].to('g*cm**2/s')
+        Lz    = sp_use[zname].to('g*cm**2/s')
+        R     = sp_use[rname].to('kpc')
+        mass     = sp_use[mname].to('Msun')
+
+        Ltot = np.sqrt(Lx**2. + Ly**2. + Lz**2.)
+        
+        L_dic[mtype]['adist']['r']       = R
+        L_dic[mtype]['adist']['phil']    = np.arccos(Lz/Ltot)*180./pi
+        L_dic[mtype]['adist']['thel']    = np.arctan2(Ly,Lx)*180./pi
+        L_dic[mtype]['adist']['ltot']    = Ltot
+        L_dic[mtype]['adist']['mass']    = mass
+
+    if save:
+        fname = '%s/angular_momentum/profiles/%s/Lprof_%s_%s.npy'%(args.save_dir, args.halo, args.halo, args.output)
+        np.save(fname, L_dic)
+        print ('saved L dictionary to %s...'%fname)
+    return L_dic
+
 
 if __name__ == '__main__':
     args = parse_args()
     ds, refine_box = load_sim(args)
+
+
+
+    mass_types = [(0., 1.5e4, 'cold', 'darkblue'),
+                 (1.5e4, 1.e5, 'warm', 'blue'),
+                 (1.e5, 1.e6, 'warmhot', 'red'),
+                 (1.e6, 1.e10, 'hot', 'darkred'), 
+                 (-1., -1., 'stars', 'goldenrod'),
+                 (-1., -1., 'dm', 'black')]
+
+    disk_Lhat, bulk_vel = find_disk_Lhat(ds)
+    L_dic               = radial_profile(ds, bulk_vel, mass_types, args)
+
+
+
+
+
+
+
+
+
+
+
+
 
     '''
     make_projection_plots(ds = refine_box.ds, center = ds.halo_center_kpc,
                           refine_box = refine_box, x_width = 20.*kpc, 
                           fig_dir = '/Users/rsimons/Desktop', haloname = args.halo, name = args.run, 
                           fig_end = 'projection', do = ['temp'], axes = ['x', 'y', 'z'], is_central = True, add_arrow = False)
-    '''
-    sp = ds.sphere(ds.halo_center_kpc, (5, 'kpc'))
-    sp.set_field_parameter('bulk_velocity', ds.halo_velocity_kms)
-    results = {}
-    results['L'] = []
 
-    for i in ['x', 'y', 'z']:
-        L_gas     = sp.quantities.total_quantity(('gas', 'angular_momentum_%s'%i))
-        print (i, L_gas)
-        results['L'].append(float(L_gas.to('cm**2*g/s').value))
-
-    results['L'] = np.array(results['L'])
-
-    L = results['L']/np.sqrt(np.sum(results['L']**2.))
     
     sp_sm = ds.sphere(ds.halo_center_kpc, (15., 'kpc'))
 
@@ -150,6 +246,7 @@ if __name__ == '__main__':
             prj.save('/user/rsimons/foggie/angular_momentum/movies/new_%i.png'%d)
 
 
+    '''
 
 
 
