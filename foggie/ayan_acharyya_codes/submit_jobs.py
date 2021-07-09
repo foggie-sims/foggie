e1dfa0f1c540e272056d5196ee1349c642fdd459
ayanacharyya <acharyyaayan@gmail.com>
2021-07-09 00:14:04 -0400

    bunch of first commits

---
+++ foggie/ayan_acharyya_codes/submit_jobs.py
@@ -0,0 +1,114 @@
+##!/usr/bin/env python3
+
+"""
+
+    Title :      submit_jobs
+    Notes :      Python wrapper to create and submit one or more jobs on pleiades
+    Author :     Ayan Acharyya
+    Started :    July 2021
+    Example :    run submit_jobs.py --call filter_star_properties --prefix fsp --do_all_sims --nnodes 50 --proc ivy --queue devel --dryrun
+
+"""
+import numpy as np
+import os, subprocess, argparse, datetime
+HOME = os.getenv('HOME')
+
+# ---------------------------------------------------------
+def parse_args():
+    '''
+    Function to parse keyword arguments
+    '''
+    parser = argparse.ArgumentParser(description="calling plotobservables for full parameter space")
+    parser.add_argument('--system', metavar='system', type=str, action='store', default='ayan_pleiades')
+    parser.add_argument('--queue', metavar='queue', type=str, action='store', default='normal')
+    parser.add_argument('--nnodes', metavar='nnodes', type=int, action='store', default=1)
+    parser.add_argument('--ncores', metavar='ncores', type=int, action='store', default=None)
+    parser.add_argument('--proc', metavar='proc', type=str, action='store', default='has')
+    parser.add_argument('--start', metavar='start', type=int, action='store', default=1)
+    parser.add_argument('--stop', metavar='stop', type=int, action='store', default=1)
+    parser.add_argument('--mergeHII', metavar='mergeHII', type=float, action='store', default=None)
+    parser.add_argument('--proj', metavar='proj', type=str, action='store', default='s1938')
+    parser.add_argument('--prefix', metavar='prefix', type=str, action='store', default=None)
+    parser.add_argument('--callfunc', metavar='callfunc', type=str, action='store', default='filter_star_properties')
+    parser.add_argument('--dryrun', dest='dryrun', action='store_true', default=False)
+    parser.add_argument('--do_all_sims', dest='do_all_sims', action='store_true', default=False)
+    args, leftovers = parser.parse_known_args()
+
+    return args
+
+# ---------------------------------------------------------
+if __name__ == '__main__':
+    time_of_begin = datetime.datetime.now()
+    args = parse_args()
+
+    #----------setting different variables based on args--------
+
+    systemflag = ' --system ' + args.system
+    dryrunflag = ' --dryrun ' if args.dryrun else ''
+    do_all_simsflag = ' --do_all_sims ' if args.do_all_sims else ''
+    mergeHIIflag = ' --mergeHII ' + str(args.mergeHII) if args.mergeHII is not None else ''
+    prefixtext = args.prefix + '_' if args.prefix is not None else ''
+
+    jobscript_path = HOME+'/Work/astro/ayan_codes/foggie/foggie/ayan_acharyya_codes/'
+    jobscript_template = 'jobscript_template_' + args.system + '.txt'
+    callfile = jobscript_path + args.callfunc + '.py'
+
+    if args.system == 'avatar':
+        workdir = '/avatar/acharyya/enzo_models' # for avatar
+        nnodes = '3' if not args.dryrun else '01'
+        ncores = '28' if not args.dryrun else '01'
+        memory = 'not-needed-for-avatar'
+        if args.queue == 'small': qname = 'smallmem'
+        elif args.queue == 'large': qname = 'largemem'
+    
+    elif args.system == 'raijin':
+        workdir = '/short/ek9/axa100/enzo_models' # for raijin
+        nnodes = 'not-needed-for-raijin'
+        ncores = '84' #'128' if not args.dryrun else '01' # 16 cores * 8 nodes
+        sizepercore = 8 if args.queue == 'large' else 4
+        if args.dryrun: sizepercore = 2
+        memory = '768GB' if args.queue == 'large' else '768GB' #str(int(ncores)*int(sizepercore))+'GB'
+        qname = 'normalbw' #'normal' if not args.dryrun else 'express'
+    
+    elif args.system == 'gadi':
+        workdir = '/scratch/ek9/axa100/enzo_models' # for gadi
+        nnodes = 2
+        ncores = 1 if args.nonewcube else str(nnodes * 48)
+        sizepercore = 4
+        if args.dryrun: sizepercore = 2
+        if args.nonewcube: memory = '30GB' if args.queue == 'large' else '8GB'
+        else: memory = str(int(ncores)*int(sizepercore))+'GB'
+        qname = 'hugemem' if args.queue == 'large' and args.nonewcube else 'normal'
+
+    elif 'pleiades' in args.system:
+        procs_dir = {'san':(16, 32), 'ivy':(20, 64), 'has':(24, 128), 'bro':(28, 128), 'bro_ele':(28, 128), 'sky_ele':(40, 192), 'cas_ait':(40, 192)} # (nnodes, mem) for each proc, from https://www.nas.nasa.gov/hecc/support/kb/pbs-resource-request-examples_188.html
+        max_hours_dict = {'low':4, 'normal':8, 'long':120, 'debug':2, 'devel':2} # from https://www.nas.nasa.gov/hecc/support/kb/pbs-job-queue-structure_187.html
+        workdir = jobscript_path # for pleiades
+        nnodes = args.nnodes
+        ncores = args.ncores if args.ncores is not None else procs_dir[args.proc][0]
+        if args.dryrun: memory = str(int(ncores) * 2) + 'GB'
+        else: memory = str(int(nnodes) * procs_dir[args.proc][1]) + 'GB'
+        qname = args.queue
+
+    #----------looping over and creating + submitting job files--------
+
+    for jobid in range(args.start, args.stop+1):
+        jobname = prefixtext + 'job' + str(jobid)
+        nhours = '01' if args.dryrun or args.queue == 'devel' else '%02d'%(max_hours_dict[args.queue])
+
+        out_jobscript = jobscript_path + 'jobscript_' + jobname + '.sh'
+
+        replacements = {'PROJ_CODE': args.proj, 'RUN_NAME': jobname, 'NNODES': nnodes, 'NHOURS': nhours, 'CALLFILE': callfile, 'WORKDIR': workdir, \
+                        'JOBSCRIPT_PATH':jobscript_path, 'NCORES': ncores, 'MEMORY': memory, 'DRYRUNFLAG': dryrunflag, 'QNAME': qname, 'PROC':args.proc, \
+                        'MERGEHIIFLAG': mergeHIIflag, 'DO_ALL_SIMSFLAG':do_all_simsflag, 'SYSTEMFLAG':systemflag, 'NCPUS':nnodes * ncores} # keywords to be replaced in template jobscript
+
+        with open(jobscript_path + jobscript_template) as infile, open(out_jobscript, 'w') as outfile:
+            for line in infile:
+                for src, target in replacements.items():
+                    line = line.replace(str(src), str(target))
+                outfile.write(line) # replacing and creating new jobscript file
+
+        print('Going to submit job ' + jobname+' at {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+        jobid = subprocess.check_output(['qsub ' + out_jobscript], shell=True)[:-1]
+
+    print('Submitted all '+str(args.stop - args.start + 1)+' job/s from index '+str(args.start)+' to '+str(args.stop) + '\n')
