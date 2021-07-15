4f5ebae4e39444a7d7ef497ed47f3e320957ae4c
ayanacharyya <acharyyaayan@gmail.com>
2021-07-14 21:00:06 -0400

    now can plot LoS velocity

--- foggie/ayan_acharyya_codes/projection_plot.py
+++ foggie/ayan_acharyya_codes/projection_plot.py
@@ -7,7 +7,7 @@
     Output :     projection plots as png files
     Author :     Ayan Acharyya
     Started :    January 2021
-    Example :    run projection_plot.py --system ayan_hd --halo 4123 --output RD0038 --do gas --proj x --fullbox --nrot 0 --do_central
+    Example :    run projection_plot.py --system ayan_hd --halo 4123 --output RD0038 --do gas --proj x --fullbox --nrot 0 --iscolorlog
 
 """
 from header import *
@@ -23,17 +23,16 @@ def do_plot(ds, field, axs, annotate_positions, small_box, center, box_width, cm
     Function to make yt projection plot
     (adopted from foggie.satellites.for_paper.central_projection_plots)
     '''
-    box_width_kpc = box_width * kpc
-    box_width_code = box_width / get_proper_box_size(ds) # converting from kpc to code units
+    box_width_code = (box_width/kpc) / get_proper_box_size(ds) # converting from kpc to code units
     if noweight: weight_field = None
 
     start_time2 = time.time()
     if field[1] == 'age': # then do ParticlePlot
-        prj = yt.ParticleProjectionPlot(ds, axs, field, center=center, data_source=small_box, width=box_width_kpc, weight_field=weight_field)
+        prj = yt.ParticleProjectionPlot(ds, axs, field, center=center, data_source=small_box, width=box_width, weight_field=weight_field)
     elif normal_vector is not None: # do rotated off axis ProjectionPlot
         prj = yt.OffAxisProjectionPlot(ds, normal_vector, field, north_vector=north_vector, center=center, width=box_width_code, data_source=small_box, weight_field=weight_field)
     else: # do ProjectionPlot
-        prj = yt.ProjectionPlot(ds, axs, field, center=center, data_source=small_box, width=box_width_kpc, weight_field=weight_field)
+        prj = yt.ProjectionPlot(ds, axs, field, center=center, data_source=small_box, width=box_width, weight_field=weight_field)
 
     print('Just the plotting took %s minutes' % ((time.time() - start_time2) / 60))
 
@@ -77,12 +76,13 @@ def make_projection_plots(ds, center, refine_box, box_width, fig_dir, name, \
                     center[2] - box_width / 2.: center[2] + box_width / 2., ]
 
     # The variables used below come from foggie.utils.consistency.py
-    field_dict = {'gas':('gas', 'density'), 'gas_entropy':('gas', 'entropy'), 'stars':('deposit', 'stars_density'),'ys_density':('deposit', 'young_stars_density'), 'ys_age':('my_young_stars', 'age'), 'ys_mass':('deposit', 'young_stars_mass'), 'metal':('gas', 'metallicity'), 'temp':('gas', 'temperature'), 'dm':('deposit', 'dm_density'), 'vrad':('gas', 'radial_velocity_corrected')}
-    cmap_dict = {'gas':density_color_map, 'gas_entropy':entropy_color_map, 'stars':plt.cm.Greys_r, 'ys_density':density_color_map, 'ys_age':density_color_map, 'ys_mass':density_color_map, 'metal':metal_color_map, 'temp':temperature_color_map, 'dm':plt.cm.gist_heat, 'vrad':velocity_discrete_cmap}
-    unit_dict = defaultdict(lambda: 'Msun/pc**2', metal='Zsun', temp='K', vrad='km/s', ys_age='Myr', ys_mass='pc*Msun', gas_entropy='keV*cm**3')
-    zmin_dict = defaultdict(lambda: density_proj_min, metal=2.5e-1, temp=1.e3, vrad=-250, ys_age=0.1, ys_mass=1, ys_density=1e-3, gas_entropy=1.6e25)
-    zmax_dict = defaultdict(lambda: density_proj_max, metal= metal_max, temp= temperature_max, vrad=250, ys_age=10, ys_mass=2e3, ys_density=1e1, gas_entropy=1.2e27)
-    weight_field_dict = defaultdict(lambda: None, metal=('gas', 'density'), temp=('gas', 'density'), vrad=('gas', 'density'))
+    field_dict = {'gas':('gas', 'density'), 'gas_entropy':('gas', 'entropy'), 'stars':('deposit', 'stars_density'),'ys_density':('deposit', 'young_stars_density'), 'ys_age':('my_young_stars', 'age'), 'ys_mass':('deposit', 'young_stars_mass'), 'metal':('gas', 'metallicity'), 'temp':('gas', 'temperature'), 'dm':('deposit', 'dm_density'), 'vrad':('gas', 'radial_velocity_corrected'), 'vlos':('gas', 'v_corrected')}
+    cmap_dict = {'gas':density_color_map, 'gas_entropy':entropy_color_map, 'stars':plt.cm.Greys_r, 'ys_density':density_color_map, 'ys_age':density_color_map, 'ys_mass':density_color_map, 'metal':metal_color_map, 'temp':temperature_color_map, 'dm':plt.cm.gist_heat, 'vrad':velocity_discrete_cmap, 'vlos':velocity_discrete_cmap}
+    unit_dict = defaultdict(lambda: 'Msun/pc**2', metal='Zsun', temp='K', vrad='km/s', ys_age='Myr', ys_mass='pc*Msun', gas_entropy='keV*cm**3', vlos='km/s')
+    zmin_dict = defaultdict(lambda: density_proj_min, metal=2e-2, temp=1.e3, vrad=-50, ys_age=0.1, ys_mass=1, ys_density=1e-3, gas_entropy=1.6e25, vlos=-50)
+    zmax_dict = defaultdict(lambda: density_proj_max, metal= 5e0, temp= temperature_max, vrad=50, ys_age=10, ys_mass=2e3, ys_density=1e1, gas_entropy=1.2e27, vlos=50)
+    weight_field_dict = defaultdict(lambda: None, metal=('gas', 'density'), temp=('gas', 'density'), vrad=('gas', 'density'), vlos=('gas', 'density'))
+    colorlog_dict = defaultdict(lambda: False, metal=True, gas=True, temp=True, gas_entropy=True)
     north_vector_dict = {'x':[0,1,0], 'y':[0,0,1], 'z':[1,0,0]} # north vector = which way is up; therefore, for proj = x, i.e. LoS _along_ x-axis (with 0 rot), up direction is actually y-axis, and for proj=y, z axis is 'up' direction
     normal_vector_dict = {'x': [np.cos(2 * np.pi * rot_frame / nframes), 0, np.sin(2 * np.pi * rot_frame / nframes)], \
                           'y': [np.sin(2 * np.pi * rot_frame / nframes), np.cos(2 * np.pi * rot_frame / nframes), 0], \
@@ -97,7 +97,7 @@ def make_projection_plots(ds, center, refine_box, box_width, fig_dir, name, \
         for d in do:
             zmin = zmin_dict[d] if args.cmin is None else args.cmin
             zmax = zmax_dict[d] if args.cmax is None else args.cmax
-            prj = do_plot(ds, field_dict[d], ax, annotate_positions, small_box, center, box_width, cmap_dict[d], name, unit=unit_dict[d], zmin=zmin, zmax=zmax, weight_field=weight_field_dict[d], normal_vector=normal_vector, north_vector=north_vector, hide_axes=hide_axes, iscolorlog=iscolorlog, noweight=noweight)
+            prj = do_plot(ds, 'v' + ax + '_corrected' if d == 'vlos' else field_dict[d], ax, annotate_positions, small_box, center, box_width, cmap_dict[d], name, unit=unit_dict[d], zmin=zmin, zmax=zmax, weight_field=weight_field_dict[d], normal_vector=normal_vector, north_vector=north_vector, hide_axes=hide_axes, iscolorlog=iscolorlog if iscolorlog else colorlog_dict[d], noweight=noweight)
 
             if add_velocity: prj.annotate_velocity(factor=20)
             if add_arrow:
@@ -141,7 +141,6 @@ if __name__ == '__main__':
             myprint('ds ' + str(ds) + ' for halo ' + str(this_sim[0]) + ' was already loaded at some point by utils; using that loaded ds henceforth', args)
         else:
             ds, refine_box = load_sim(args, region='refine_box', do_filter_particles=False)
-        ad = ds.all_data()
 
         fig_dir = args.output_dir + 'figs/' + args.output + '/'
         Path(fig_dir).mkdir(parents=True, exist_ok=True)
@@ -150,7 +149,7 @@ if __name__ == '__main__':
         ds.add_particle_filter('my_young_stars')
 
         if args.fullbox: args.galrad = ds.refine_width / 2 # kpc
-        center = refine_box.center.in_units(kpc) if args.do_central else args.halo_center
+        center = refine_box.center.in_units(kpc) if args.do_central else ds.arr(args.halo_center, kpc)
 
         if not args.noplot:
             if args.makerotmovie:
@@ -162,7 +161,7 @@ if __name__ == '__main__':
             for nrot in range(min_nrot, max_nrot):
                 print('Plotting', nrot + 1, 'out of', max_nrot, 'frames..')
                 prj = make_projection_plots(ds=refine_box.ds, center=center, \
-                                        refine_box=refine_box, box_width=2 * args.galrad, \
+                                        refine_box=refine_box, box_width=2 * args.galrad * kpc, \
                                         fig_dir=fig_dir, name=halo_dict[args.halo], \
                                         fig_end='projection', do=[ar for ar in args.do.split(',')], axes=[ar for ar in args.projection.split(',')], \
                                         is_central=args.do_central, add_arrow=args.add_arrow, add_velocity=args.add_velocity, rot_frame=nrot, \
