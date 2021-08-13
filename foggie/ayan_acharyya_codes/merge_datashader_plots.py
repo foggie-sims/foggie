6c4df44f94ec2c81ac85656d9ae7f84cf0e1af12
ayanacharyya <acharyyaayan@gmail.com>
2021-08-12 22:14:20 -0400

    can now combine multiple datashader images into one

--- foggie/ayan_acharyya_codes/merge_datashader_plots.py
+++ foggie/ayan_acharyya_codes/merge_datashader_plots.py
@@ -3,13 +3,13 @@
 """
 
     Title :      merge_datashader_plots
-    Notes :      Merge existing dataframes (potentially of different snapshots/halos) into a single datashader plot of 3 given quantities
-                 This script assumes that all the relevant dataframes already exist. If a particular dataframe doesn't exist, it simply skips over that snapshot
+    Notes :      Merge existing datashader plots (potentially of different snapshots/halos) into a single grid plot
+                 This script assumes that all the relevant plots already exist. If a particular plot doesn't exist, it simply skips over that snapshot
     Output :     a single datashader plot as png file
     Author :     Ayan Acharyya
     Started :    August 2021
     Examples :   run merge_datashader_plots.py --system ayan_pleiades --do_all_halos --fullbox --xcol rad --ycol metal --colorcol vrad --output RD0020,RD0018,RD0016
-                 run merge_datashader_plots.py --system ayan_local --halo 8508,4123 --fullbox --xcol rad --ycol metal --colorcol vrad --output RD0030
+                 run merge_datashader_plots.py --system ayan_local --do_all_halos --fullbox --xcol rad --ycol metal --colorcol vrad --output RD0020,RD0018,RD0016 --inflow_only
 """
 from header import *
 from util import *
@@ -25,12 +25,9 @@ if __name__ == '__main__':
     if args.xcol == 'radius': args.xcol == 'rad'
     if not args.keep: plt.close('all')
 
-    if args.do_all_sims:
-        list_of_sims = get_all_sims(args) # all snapshots of this particular halo
-    else:
-        if args.do_all_halos: halos = get_all_halos(args)
-        else: halos = args.halo_arr
-        list_of_sims = list(itertools.product(halos, args.output_arr))
+    if args.do_all_halos: halos = get_all_halos(args)
+    else: halos = args.halo_arr
+    list_of_sims = list(itertools.product(halos, args.output_arr))
     total_snaps = len(list_of_sims)
 
     if args.fullbox:
@@ -58,55 +55,44 @@ if __name__ == '__main__':
     outputs_text = 'all' if args.do_all_sims else ','.join(args.output_arr)
     inflow_outflow_text = '_inflow_only' if args.inflow_only else '_outflow_only' if args.outflow_only else ''
 
+    crop_x1, crop_x2, crop_y1, crop_y2 = 75, -1, 0, -1
+    nrow, ncol, figsize = len(args.output_arr), len(halos), (12, 6)
+
     # ----------collating the different dataframes (correpsonding to each snapshot)-------------------------------------
     for index, thiscolorcol in enumerate(colorcol_arr):
         args.colorcol = thiscolorcol
         args.colorcolname = 'log_' + args.colorcol if islog_dict[args.colorcol] else args.colorcol
         if isfield_weighted_dict[args.colorcol] and args.weight: args.colorcolname += '_wtby_' + args.weight
         args.colorcol_cat = 'cat_' + args.colorcolname
-        print_mpi('Plotting ' + args.xcolname + ' vs ' + args.ycolname + ', color coded by ' + args.colorcolname + ' i.e., plot ' + str(index + 1) + ' of ' + str(len(colorcol_arr)) + '..', args)
+        print_mpi('Combining ' + args.xcolname + ' vs ' + args.ycolname + ', color coded by ' + args.colorcolname + ' i.e., plot ' + str(index + 1) + ' of ' + str(len(colorcol_arr)) + '..', args)
 
-        thisfilename = fig_dir + 'merged_datashader_%s_%s_vs_%s_colby_%s_halos_%s_outputs_%s%s.png' % (galrad_text, args.ycolname, args.xcolname, args.colorcolname, halos_text, outputs_text, inflow_outflow_text)
+        fig, axes = plt.subplots(nrow, ncol, figsize=figsize)
+        fig.subplots_adjust(hspace=0, wspace=0, right=1, top=1, bottom=0, left=0)
 
-        df_merged, paramlist_merged = pd.DataFrame(), pd.DataFrame()
         for index2, this_sim in enumerate(list_of_sims):
             start_time_this_snapshot = time.time()
             halo, output = this_sim[0], this_sim[1]
-            print_mpi('Reading dataframe ' + output + ' of halo ' + halo + ' which is ' + str(index2 + 1) + ' out of the total ' + str(total_snaps) + ' snapshots...', args)
+            print_mpi('Reading plot ' + output + ' of halo ' + halo + ' which is ' + str(index2 + 1) + ' out of the total ' + str(total_snaps) + ' plots...', args)
 
             thisboxrad = z_boxrad_dict[output] if args.fullbox else args.galrad
-            file = output_dir.replace(args.halo, halo) + 'txtfiles/' + output + '_df_boxrad_' + str(thisboxrad) + 'kpc.txt'
+            file = output_dir.replace(args.halo, halo) + 'figs/' + output + '/datashader_boxrad_%skpc_%s_vs_%s_colby_%s%s.png' % (thisboxrad, args.ycolname, args.xcolname, args.colorcolname, inflow_outflow_text)
 
-            if os.path.exists(file):
-                df = pd.read_table(file, delim_whitespace=True, comment='#')
-            else:
+            if not os.path.exists(file):
                 myprint('Cannot find ' + file + '; skipping halo ' + halo + ' snapshot ' + output + '..' , args)
                 continue
 
-            df = extract_columns_from_df(df, args)
-            df_merged = df_merged.append(df)
-
-            if args.overplot_stars:
-                args.output_dir = output_dir.replace(args.halo, halo)
-                args.output = output
-                paramlist = get_stars_file(args)
-                paramlist_merged.append(paramlist)
+            image = mpimg.imread(file)
+            rowid, colid = int(index2 / ncol), index2 % ncol
+            axes[rowid][colid].axis('off')
+            axes[rowid][colid].imshow(image[crop_y1 : crop_y2, crop_x1 if colid else 0: crop_x2], origin='upper')
+            axes[rowid][colid].set_aspect('auto')
 
             myprint('This snapshot ' + output + ' completed in %s minutes' % ((time.time() - start_time_this_snapshot) / 60), args)
 
-        # -----------plotting one datashader plot with the giant merged dataframe-------------------------------------------
-        if len(df_merged) > 0:
-            if not os.path.exists(thisfilename) or args.clobber_plot:
-                if not os.path.exists(thisfilename):
-                    print_mpi(thisfilename + ' plot does not exist. Creating afresh..', args)
-                elif args.clobber_plot:
-                    print_mpi(thisfilename + ' plot exists but over-writing..', args)
+        thisfilename = fig_dir + 'combined_datashader_%s_%s_vs_%s_colby_%s_halos_%s_outputs_%s%s.png' % (galrad_text, args.ycolname, args.xcolname, args.colorcolname, halos_text, outputs_text, inflow_outflow_text)
+        fig.savefig(thisfilename, dpi=500)
+        myprint('Saved plot as ' + thisfilename, args)
+        plt.show(block=False)
 
-                df_merged, fig = make_datashader_plot(df_merged, thisfilename, args, npix_datashader=1000, paramlist=paramlist_merged)
-            else:
-                myprint('Skipping colorcol ' + thiscolorcol + ' because plot already exists (use --clobber_plot to over-write) at ' + thisfilename, args)
-        else:
-            myprint('Merged dataframe is empty, skipping plotting step.', args)
-
-    myprint('Serially: time taken for merging and datashading ' + str(total_snaps) + ' snapshots was %s mins' % ((time.time() - start_time) / 60), args)
+    myprint('Serially: time taken for combining ' + str(total_snaps) + ' plots was %s mins' % ((time.time() - start_time) / 60), args)
 
