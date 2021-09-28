566949cabe1b59190b7947d0229f8f965909e9fa
ayanacharyya <acharyyaayan@gmail.com>
2021-09-27 23:01:44 -0400

    path debugging

--- foggie/utils/get_run_loc_etc.py
+++ foggie/utils/get_run_loc_etc.py
@@ -102,37 +102,28 @@ def get_run_loc_etc(args):
 
     if not args.pwd:
         if args.run == "natural":
-            run_loc = "halo_00"+ args.halo + "/nref11n/"
-            trackname = code_path + "halo_tracks/00"+ args.halo +"/nref11n_selfshield_15/halo_track_200kpc_nref9"
-            infofile = code_path  + "halo_infos/00" + args.halo +"/nref11n/halo_info"
-            haloname = "halo_00"+ args.halo + "_nref11n"
-            path_part = run_loc
-            output_dir = output_path + "plots_"+path_part
-            spectra_dir = output_dir+"spectra/"
+            runname = "nref11n"
         elif args.run == "nref10f" or args.run == "nref11n_nref10f":
-            run_loc = "halo_00"+ args.halo + "/nref11n_nref10f/"
-            trackname = code_path + "halo_tracks/00"+ args.halo +"/nref11n_selfshield_15/halo_track_200kpc_nref10"
-            infofile = code_path  + "halo_infos/00" + args.halo +"/nref11n_nref10f/halo_info"
-            haloname = "halo_00"+ args.halo + "_nref11n_nref10f"
-            path_part = run_loc
-            output_dir = output_path + "plots_"+path_part
-            spectra_dir = output_dir+"spectra/"
+            runname = "nref11n_nref10f"
         elif args.run == "nref11c_nref9f" or args.run == "nref11c":
-            run_loc = "halo_00"+ args.halo + "/nref11c_nref9f/"
-            trackname = code_path + "halo_tracks/00"+ args.halo +"/nref11n_selfshield_15/halo_track_200kpc_nref9"
-            infofile = code_path  + "halo_infos/00" + args.halo +"/nref11c_nref9f/halo_info"
-            haloname = "halo_00"+ args.halo + "_nref11c_nref9f"
-            path_part = run_loc
-            output_dir = output_path + "plots_"+path_part
-            spectra_dir = output_dir+"spectra/"
+            runname = "nref11c_nref9f"
         elif 'feedback' in args.run:
-            run_loc = args.run + '/'
-            trackname = code_path + "halo_tracks/00"+ args.halo +"/nref11n_selfshield_15/halo_track_200kpc_nref9"
-            infofile = code_path  + "halo_infos/00" + args.halo +"/nref11c_nref9f/halo_info"
-            haloname = "halo_00"+ args.halo + "_" + args.run
-            path_part = run_loc
-            output_dir = output_path + "plots_"+path_part
-            spectra_dir = output_dir+"spectra/"
+            runname = "nref11c_nref9f"
+        else:
+            runname = args.run
+
+        trackname = code_path + "halo_tracks/00" + args.halo + "/nref11n_selfshield_15/halo_track_200kpc_nref9"
+        infofile = code_path  + "halo_infos/00" + args.halo + "/" + runname + "/halo_info"
+        haloname = "halo_00" + args.halo + "_" + runname
+
+        if 'feedback' in args.run:
+            run_loc = args.run + "/"
+        else:
+            run_loc = "halo_00" + args.halo + "/" + runname + "/"
+
+        output_dir = output_path + "plots_" + run_loc
+        spectra_dir = output_dir + "spectra/"
+
         if args.system=='cassiopeia' or args.system=='pleiades_cassi':
             output_dir = output_path
 
