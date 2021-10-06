2ee76180b1b6d55ce043c076c35b79422789230e
ramonaaugustin <raugustin@stsci.edu>
2021-10-06 11:22:47 -0400

    units correction

--- foggie/clumps/clump_properties.py
+++ foggie/clumps/clump_properties.py
@@ -245,7 +245,7 @@ for boxi in ['box1','box2','box3','box4','box5','box6','box7','box8','box9','box
     col21 = fits.Column(name='centerz', format='E', unit='code_units', array=zc)
     col22 = fits.Column(name='distancefromhalocenter', format='E', unit='kpc', array=distancefromhalocenter)
     col23 = fits.Column(name='numberofcells', format='E', unit='None', array=numberofcells)
-    col24 = fits.Column(name='metallicity', format='E', unit='None', array=metallicity)
+    col24 = fits.Column(name='metallicity', format='E', unit='Zsun', array=metallicity)
 
     #coldefs = fits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, col22])
     coldefs = fits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col19, col20, col21, col22, col23, col24])
