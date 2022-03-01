9670aabeeabd5129069b886e4ca2eefa0908246e
ayanacharyya <acharyyaayan@gmail.com>
2022-03-01 14:55:23 -0500

    modified ldan memory request option

--- foggie/ayan_acharyya_codes/submit_jobs.py
+++ foggie/ayan_acharyya_codes/submit_jobs.py
@@ -9,7 +9,7 @@
     Example :    run submit_jobs.py --call filter_star_properties --do_all_sims --nnodes 50 --ncores 4 --prefix fsp --halo 8508 --dryrun
     OR :         run submit_jobs.py --call filter_star_properties --do_all_sims --nnodes 50 --ncores 4 --prefix fsp --do_all_halos --nhours 24 --dryrun
     OR :         run /nobackup/aachary2/ayan_codes/foggie/foggie/ayan_acharyya_codes/submit_jobs.py --call datashader_movie --galrad 20 --xcol rad --ycol metal --colorcol density,vrad,temp --overplot_stars --makemovie --delay 0.1 --do_all_sims --prefix rsv_dsm_Zprofs --halo 8508 --queue s1938_mpe1 --aoe sles12 --proj s1938 --nnodes 5 --ncores 4 --proc has
-    OR :         run submit_jobs.py --call compute_MZgrad --system ayan_pleiades --halo 8508 --do_all_sims --nnodes 50 --ncores 4 --queue normal --opt_args "--upto_re 3 --xcol rad_re --weight mass --write_file --noplot"
+    OR :         run submit_jobs.py --call compute_MZgrad --system ayan_pleiades --halo 8508 --do_all_sims --nnodes 50 --ncores 4 --queue normal --prefix cmzg --opt_args "--upto_re 3 --xcol rad_re --weight mass --write_file --noplot"
 """
 import subprocess, argparse, datetime, os
 from collections import defaultdict
@@ -148,7 +148,7 @@ if __name__ == '__main__':
     if args.queue[:2] == 'e_': resources += ':mem=' + memory # for submitting to endeavour
     else: resources += ':mpiprocs=' + str(ncores)
 
-    if args.queue == 'ldan': resources += ''# ':mem=' + memory # need to specify mem per node for jobs on LDAN (for other procs it is by default the max available node mem)
+    if args.queue == 'ldan': resources += ':mem=' + memory # may specify mem per node for jobs on LDAN (for other procs it is by default the max available node mem)
     else: resources += ':model=' + args.proc # need to specify the proc (but not necessarily the mem if I'm using the full node memory) if not an LDAN job
 
     if args.aoe is not None: resources += ':aoe=' + args.aoe
