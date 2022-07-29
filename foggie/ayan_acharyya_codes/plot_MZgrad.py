b21c96a3743c8c1ac5d6e369c9044506f9cf3dd2
ayanacharyya <acharyyaayan@gmail.com>
2022-07-28 23:03:49 -0400

    can now plot deviation from mean behaviour

--- foggie/ayan_acharyya_codes/plot_MZgrad.py
+++ foggie/ayan_acharyya_codes/plot_MZgrad.py
@@ -13,6 +13,7 @@
                  run plot_MZgrad.py --system ayan_pleiades --halo 8508 --upto_re 3 --Zgrad_den rad_re --weight mass --binby log_mass --nbins 20 --cmap plasma --xmax 11 --ymin 0.3 --overplot_manga --manga_diag n2
                  run plot_MZgrad.py --system ayan_local --halo 8508 --Zgrad_den kpc --upto_kpc 10 --keep --weight mass --ycol Zgrad --xcol time --colorcol log_mass --overplot_smoothed --zhighlight
                  run plot_MZgrad.py --system ayan_local --halo 8508 --Zgrad_den kpc --upto_kpc 10 --keep --weight mass --ycol Zgrad --xcol time --colorcol re --cmax 3 --zhighlight
+                 run plot_MZgrad.py --system ayan_local --halo 8508 --Zgrad_den kpc --upto_kpc 10 --keep --weight mass --ycol Zgrad --xcol log_mass --colorcol time --zhighlight --plot_deviation --zcol log_ssfr
 """
 from header import *
 from util import *
@@ -25,8 +26,11 @@ def load_df(args):
     Function to load and return the dataframe containing MZGR
     '''
     args.foggie_dir, args.output_dir, args.run_loc, args.code_path, args.trackname, args.haloname, args.spectra_dir, args.infofile = get_run_loc_etc(args)
-    upto_text = '_upto%.1Fkpc' % args.upto_kpc if args.upto_kpc is not None else '_upto%.1FRe' % args.upto_re
     Zgrad_den_text = 'rad' if args.Zgrad_den == 'kpc' else 'rad_re'
+    if args.upto_kpc is not None:
+        upto_text = '_upto%.1Fckpchinv' % args.upto_kpc if args.docomoving else '_upto%.1Fkpc' % args.upto_kpc
+    else:
+        upto_text = '_upto%.1FRe' % args.upto_re
     grad_filename = args.output_dir + 'txtfiles/' + args.halo + '_MZR_xcol_%s%s%s.txt' % (Zgrad_den_text, upto_text, args.weightby_text)
 
     convert_Zgrad_from_dexkpc_to_dexre = False
@@ -69,6 +73,8 @@ def load_df(args):
     df.columns = ['output', 'redshift', 'time', 're', 'mass', 'Zcen', 'Zcen_u', 'Zgrad', 'Zgrad_u', 'Ztotal']
     df['log_mass'] = np.log10(df['mass'])
     df = df.drop('mass', axis=1)
+
+    df['Ztotal'] = np.log10(df['Ztotal']) + 8.69
 
     if convert_Zgrad_from_dexkpc_to_dexre:
         print('Zgrad is in dex/kpc, converting it to dex/re')
@@ -171,9 +177,10 @@ def plot_MZGR(args):
     things_that_reduce_with_time = ['redshift', 're'] # whenever this quantities are used as colorcol, the cmap is inverted, so that the darkest color is towards later times
 
     # -------------get plot limits-----------------
-    lim_dict = {'Zgrad': (-0.5, 0.1)  if args.Zgrad_den == 'kpc' else (-2, 0.1), 're': (0, 30), 'log_mass': (8.5, 11.5), 'redshift': (0, 6), 'time': (0, 14)}
+    lim_dict = {'Zgrad': (-0.5, 0.1)  if args.Zgrad_den == 'kpc' else (-2, 0.1), 're': (0, 30), 'log_mass': (8.5, 11.5), 'redshift': (0, 6), 'time': (0, 14), 'sfr': (0, 60), 'log_ssfr': (-11, -8), 'Ztotal': (8, 9)}
     label_dict = MyDefaultDict(Zgrad=r'$\nabla(\log{\mathrm{Z}}$) (dex/r$_{\mathrm{e}}$)' if args.Zgrad_den == 're' else r'$\Delta Z$ (dex/kpc)', \
-        re='Scale length (kpc)', log_mass=r'$\log{(\mathrm{M}_*/\mathrm{M}_\odot)}$', redshift='Redshift', time='Time (Gyr)')
+        re='Scale length (kpc)', log_mass=r'$\log{(\mathrm{M}_*/\mathrm{M}_\odot)}$', redshift='Redshift', time='Time (Gyr)', sfr=r'SFR (M$_{\odot}$/yr)', \
+        log_ssfr=r'$\log{\, \mathrm{sSFR} (\mathrm{yr}^{-1})}$', Ztotal=r'$\log{(\mathrm{O/H})}$ + 12')
 
     if args.xmin is None: args.xmin = lim_dict[args.xcol][0]
     if args.xmax is None: args.xmax = lim_dict[args.xcol][1]
@@ -181,10 +188,17 @@ def plot_MZGR(args):
     if args.ymax is None: args.ymax = lim_dict[args.ycol][1]
     if args.cmin is None: args.cmin = lim_dict[args.colorcol][0]
     if args.cmax is None: args.cmax = lim_dict[args.colorcol][1]
+    if args.zmin is None: args.zmin = lim_dict[args.zcol][0]
+    if args.zmax is None: args.zmax = lim_dict[args.zcol][1]
 
     # -------declare figure object-------------
     fig, ax = plt.subplots(1, figsize=(12, 6))
-    fig.subplots_adjust(top=0.95, bottom=0.15, left=0.12, right=1.05)
+    fig.subplots_adjust(top=0.95, bottom=0.15, left=0.1, right=1.05)
+
+    if args.plot_deviation:
+        fig2, ax2 = plt.subplots(1, figsize=(12, 6))
+        fig2.subplots_adjust(top=0.95, bottom=0.15, left=0.1, right=1.05)
+        args.overplot_smoothed = True
 
     # ---------plot observations----------------
     obs_text = ''
@@ -215,16 +229,30 @@ def plot_MZGR(args):
     for index, args.halo in enumerate(args.halo_arr[::-1]):
         thisindex = len(args.halo_arr) - index - 1
         df = load_df(args)
-        df = df[(df[args.xcol] >= args.xmin) & (df[args.xcol] <= args.xmax)]
+        # -------- reading in additional dataframes-------
+        addn_df = pd.read_table(args.code_path + 'halo_infos/00' + args.halo + '/' + args.run + '/sfr', names=('output', 'redshift', 'sfr'), comment='#', delim_whitespace=True)
+        df = df.merge(addn_df[['output', 'sfr']], on='output')
+        df['ssfr'] = df['sfr'] / 10**df['log_mass']
+        df = df.replace([0, np.inf, -np.inf], np.nan).dropna(axis=0)
+        df['log_ssfr'] = np.log10(df['ssfr'])
+        df = df.sort_values(args.xcol)
+
+        #df = df[(df[args.xcol] >= args.xmin) & (df[args.xcol] <= args.xmax)]
         #df = df[(df[args.ycol] >= args.ymin) & (df[args.ycol] <= args.ymax)]
+        df = df[(df[args.colorcol] >= args.cmin) & (df[args.colorcol] <= args.cmax)]
 
         if args.binby is not None:
             df[args.binby + '_bins'] = pd.cut(df[args.binby], bins=np.linspace(np.min(df[args.binby]), np.max(df[args.binby]), args.nbins))
-            df = df[[args.colorcol, args.xcol, args.ycol, args.binby + '_bins']].groupby(args.binby + '_bins', as_index=False).agg(np.mean)
-            df.dropna(inplace=True)
+            cols_to_bin = [args.colorcol, args.xcol, args.ycol, args.binby + '_bins']
+            if args.plot_deviation: cols_to_bin += [args.zcol]
+            if 'redshift' not in cols_to_bin: cols_to_bin += ['redshift']
+            df = df[cols_to_bin].groupby(args.binby + '_bins', as_index=False).agg(np.mean)
+            df.dropna(axis=0, inplace=True)
 
         # -----plot line with color gradient--------
         this_cmap = cmap_arr[thisindex] + '_r' if args.colorcol in things_that_reduce_with_time else cmap_arr[thisindex] # reverse colromap for redshift
+        reversed_thiscmap = this_cmap + '_r' if '_r' not in this_cmap else this_cmap[:-2]
+        thistextcolor = mpl_cm.get_cmap(this_cmap)(0.2 if args.colorcol == 'redshift' else 0.2 if args.colorcol == 're' else 0.8)
         line = get_multicolored_line(df[args.xcol], df[args.ycol], df[args.colorcol], this_cmap, args.cmin, args.cmax, lw=1 if args.overplot_smoothed else 2)
         plot = ax.add_collection(line)
 
@@ -237,20 +265,44 @@ def plot_MZGR(args):
 
         # ------- overplotting a boxcar smoothed version of the MZGR------------
         if args.overplot_smoothed:
-            line.set_alpha(0.2) # make the actual wiggly line fainter
             npoints = int(len(df)/8)
             if npoints % 2 == 0: npoints += 1
             box = np.ones(npoints) / npoints
             df[args.ycol + '_smoothed'] = np.convolve(df[args.ycol], box, mode='same')
+
+            line.set_alpha(0.2) # make the actual wiggly line fainter
             smoothline = get_multicolored_line(df[args.xcol], df[args.ycol + '_smoothed'], df[args.colorcol], this_cmap, args.cmin, args.cmax, lw=2)
             plot = ax.add_collection(smoothline)
             print('Boxcar-smoothed plot for halo', args.halo, 'with', npoints, 'points')
 
-        fig.text(0.15, 0.9 - thisindex * 0.05, halo_dict[args.halo], ha='left', va='top', color=mpl_cm.get_cmap(this_cmap)(0.2 if args.colorcol == 'redshift' else 0.2 if args.colorcol == 're' else 0.8), fontsize=args.fontsize)
+        # ------- making additional plot of deviation in gradient vs other quantities, like SFR------------
+        if args.plot_deviation:
+            print('Plotting deviation vs', args.colorcol, 'halo', args.halo)
+            df[args.ycol + '_deviation'] = df[args.ycol] - df[args.ycol + '_smoothed']
+            df = df.sort_values(args.zcol)
+
+            # --------- scatter plot------------
+            #ax2.scatter(df[args.zcol], df[args.ycol + '_deviation'], c=thistextcolor, edgecolor='k', lw=0.5, s=50)
+
+            # --------- colored line plot------------
+            line2 = get_multicolored_line(df[args.zcol], np.abs(df[args.ycol + '_deviation']), df[args.colorcol], reversed_thiscmap, args.cmin, args.cmax, lw=1)
+            plot2 = ax2.add_collection(line2)
+
+            # --------- smoothed colored line plot------------
+            line2.set_alpha(0.2) # make the actual wiggly line fainter
+
+            df[args.ycol + '_deviation_smoothed'] = np.convolve(np.abs(df[args.ycol + '_deviation']), box, mode='same')
+            smoothline2 = get_multicolored_line(df[args.zcol], df[args.ycol + '_deviation_smoothed'], df[args.colorcol], reversed_thiscmap, args.cmin, args.cmax, lw=2)
+            plot2 = ax2.add_collection(smoothline2)
+
+
+        fig.text(0.15, 0.9 - thisindex * 0.05, halo_dict[args.halo], ha='left', va='top', color=thistextcolor, fontsize=args.fontsize)
+        if args.plot_deviation: fig2.text(0.15, 0.9 - thisindex * 0.05, halo_dict[args.halo], ha='left', va='top', color=thistextcolor, fontsize=args.fontsize)
         df['halo'] = args.halo
         df_master = pd.concat([df_master, df])
 
-    cax = plt.colorbar(plot)
+    # ------- tidying up fig1------------
+    cax = fig.colorbar(plot)
     cax.ax.tick_params(labelsize=args.fontsize)
     cax.set_label(label_dict[args.colorcol], fontsize=args.fontsize)
 
@@ -266,12 +318,33 @@ def plot_MZGR(args):
 
     binby_text = '' if args.binby is None else '_binby_' + args.binby
     upto_text = '_upto%.1Fkpc' % args.upto_kpc if args.upto_kpc is not None else '_upto%.1FRe' % args.upto_re
-    figname = args.output_dir + 'figs/' + ','.join(args.halo_arr) + '_%s_vs%s_colorby_%s_Zgrad_den_%s%s%s%s%s.png' % (args.ycol, args.xcol, args.colorcol, args.Zgrad_den, upto_text, args.weightby_text, binby_text, obs_text)
+    figname = args.output_dir + 'figs/' + ','.join(args.halo_arr) + '_%s_vs_%s_colorby_%s_Zgrad_den_%s%s%s%s%s.png' % (args.ycol, args.xcol, args.colorcol, args.Zgrad_den, upto_text, args.weightby_text, binby_text, obs_text)
     fig.savefig(figname)
     print('Saved plot as', figname)
-    plt.show(block=False)
 
-    return fig, df_master, df_manga
+    # ------- tidying up fig2 if any------------
+    if args.plot_deviation:
+        cax = fig2.colorbar(plot2)
+        cax.ax.tick_params(labelsize=args.fontsize)
+        cax.set_label(label_dict[args.colorcol], fontsize=args.fontsize)
+
+        ax2.set_xlim(args.zmin, args.zmax)
+        ax2.set_ylim(-0.01, 0.15)
+
+        ax2.set_xticklabels(['%.1F' % item for item in ax2.get_xticks()], fontsize=args.fontsize)
+        ax2.set_yticklabels(['%.2F' % item for item in ax2.get_yticks()], fontsize=args.fontsize)
+
+        ax2.set_xlabel(label_dict[args.zcol], fontsize=args.fontsize)
+        ax2.set_ylabel('Deviation in ' + label_dict[args.ycol], fontsize=args.fontsize)
+
+        figname = args.output_dir + 'figs/' + ','.join(args.halo_arr) + '_dev_in_%s_vs_%s_colorby_%s_Zgrad_den_%s%s%s%s%s.png' % (args.ycol, args.zcol, args.colorcol, args.Zgrad_den, upto_text, args.weightby_text, binby_text, obs_text)
+        fig2.savefig(figname)
+        print('Saved plot as', figname)
+    else:
+        fig2 = None
+
+    plt.show(block=False)
+    return fig, fig2, df_master, df_manga
 
 
 # -----main code-----------------
@@ -285,10 +358,10 @@ if __name__ == '__main__':
     args.weightby_text = '' if args.weight is None else '_wtby_' + args.weight
     if args.ycol == 'metal': args.ycol = 'Zgrad' # changing the default ycol to metallicity gradient
     if args.xcol == 'rad': args.xcol = 'log_mass' # changing the default xcol to mass, to make a MZGR plot by default when xcol and ycol aren't specified
-    if args.colorcol == ['vrad']: args.colorcol = 'redshift'
+    if args.colorcol == ['vrad']: args.colorcol = 'time'
     else: args.colorcol = args.colorcol[0]
 
-    fig, df_binned, df_manga = plot_MZGR(args)
+    fig, fig2, df_binned, df_manga = plot_MZGR(args)
 
     print('Completed in %s' % (datetime.timedelta(minutes=(time.time() - start_time) / 60)))
 
