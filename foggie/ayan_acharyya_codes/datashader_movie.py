21ad93b9a32c5ed79d6335e1623112c24f159e65
ayanacharyya <acharyyaayan@gmail.com>
2022-07-29 18:47:23 -0400

    minor bug fix

--- foggie/ayan_acharyya_codes/datashader_movie.py
+++ foggie/ayan_acharyya_codes/datashader_movie.py
@@ -99,7 +99,7 @@ def extract_columns_from_df(df_allprop, args):
     return df
 
 # -------------------------------------------------------------------------------
-def get_df_from_ds(ds, args):
+def get_df_from_ds(ds, args, outfilename=None):
     '''
     Function to make a pandas dataframe from the yt dataset based on the given field list and color category,
     then writes dataframe to file for faster access in future
@@ -108,7 +108,7 @@ def get_df_from_ds(ds, args):
     '''
     # -------------read/write pandas df file with ALL fields-------------------
     Path(args.output_dir + 'txtfiles/').mkdir(parents=True, exist_ok=True)  # creating the directory structure, if doesn't exist already
-    outfilename = get_correct_tablename(args)
+    if outfilename is None: outfilename = get_correct_tablename(args)
 
     all_fields = [args.xcol, args.ycol, args.colorcol] if args.quick else field_dict.keys()  # only the relevant properties if in a hurry
     if 'rad' not in all_fields: all_fields = ['rad'] + all_fields
@@ -135,9 +135,12 @@ def get_df_from_ds(ds, args):
 
         if not set(all_fields).issubset(set(df_allprop.columns)): # if existing file was old, it might not have all the required columns, in which case clobber to make new file now
             dummy_args = copy.deepcopy(args)
+            if dummy_args.quick: outfilename = dummy_args.output_dir + 'txtfiles/' + dummy_args.output + '_df_boxrad_%.2Fkpc_%s_vs_%s_colby_%s%s.txt' % (dummy_args.galrad, dummy_args.ycol, dummy_args.xcol, dummy_args.colorcol, inflow_outflow_text)
+            else: outfilename = dummy_args.output_dir + 'txtfiles/' + dummy_args.output + '_df_boxrad_%.2Fkpc.txt' % (dummy_args.galrad)
+
             dummy_args.clobber = True
             myprint(outfilename + ' exists but does not have all the required fields..', dummy_args)
-            df_allprop = get_df_from_ds(ds, dummy_args)
+            df_allprop = get_df_from_ds(ds, dummy_args, outfilename=outfilename)
 
         df_allprop = df_allprop[df_allprop['rad'].between(0, args.galrad)] # curtailing in radius space, in case this dataframe has been read in from a file corresponding to a larger chunk of the box
 
