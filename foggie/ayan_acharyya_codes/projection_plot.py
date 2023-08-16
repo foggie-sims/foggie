308dc816144508ef94fd467a4dc47488d731b770
ayanacharyya <acharyyaayan@gmail.com>
2023-08-16 12:43:35 -0400

    can now annotate positions

--- foggie/ayan_acharyya_codes/projection_plot.py
+++ foggie/ayan_acharyya_codes/projection_plot.py
@@ -45,7 +45,7 @@ def annotate_box(p, width, ds, center, unit='kpc', projection='x'):
 
 # --------------------------------------------------------------------------------
 def do_plot(ds, field, axs, annotate_positions, small_box, center, box_width, cmap, name, unit='Msun/pc**2', \
-            zmin=density_proj_min, zmax=density_proj_max, ann_sphere_rad=(1, 'kpc'), weight_field=None, \
+            zmin=density_proj_min, zmax=density_proj_max, ann_sphere_rad=(1, 'kpc'), weight_field=None, annotate_markers=[], \
             normal_vector=None, north_vector=None, hide_axes=False, iscolorlog=False, noweight=False, fontsize=20, args=None):
     '''
     Function to make yt projection plot
@@ -93,11 +93,14 @@ def do_plot(ds, field, axs, annotate_positions, small_box, center, box_width, cm
     for cen in annotate_positions:
         prj.annotate_sphere(cen, radius=ann_sphere_rad, coord_system='data', circle_args={'color': 'white'})
 
+    for cen in annotate_markers:
+        prj.annotate_marker(cen, coord_system='data')
+
     return prj
 
 # --------------------------------------------------------------------------------
 def make_projection_plots(ds, center, refine_box, box_width, fig_dir, name, \
-                          fig_end='projection', do=['stars', 'gas', 'metal'], projections=['x', 'y', 'z'], annotate_positions=[], \
+                          fig_end='projection', do=['stars', 'gas', 'metal'], projections=['x', 'y', 'z'], annotate_positions=[], annotate_markers=[], \
                           is_central=False, add_velocity=False, add_arrow=False, start_arrow=[], end_arrow=[], total_normal_rot=0, \
                           total_north_rot=0, rot_frame=0, nframes=200, hide_axes=False, iscolorlog=False, noweight=False, \
                           rot_north_about='x', rot_normal_about='y', output='', fontsize=20, cbar_horizontal=False, use_density_cut=False, args=None):
@@ -153,7 +156,7 @@ def make_projection_plots(ds, center, refine_box, box_width, fig_dir, name, \
             zmax = zmax_dict[d] if args.cmax is None else args.cmax
 
             thisfield = 'v' + thisproj + '_corrected' if d == 'vlos' else field_dict[d]
-            prj = do_plot(ds, thisfield, thisproj, annotate_positions, small_box, center, box_width, cmap_dict[d], name, unit=unit_dict[d], zmin=zmin, zmax=zmax, weight_field=weight_field_dict[d], normal_vector=normal_vector, north_vector=north_vector, hide_axes=hide_axes, iscolorlog=iscolorlog if iscolorlog else colorlog_dict[d], noweight=noweight, fontsize=fontsize, args=args)
+            prj = do_plot(ds, thisfield, thisproj, annotate_positions, small_box, center, box_width, cmap_dict[d], name, unit=unit_dict[d], zmin=zmin, zmax=zmax, weight_field=weight_field_dict[d], normal_vector=normal_vector, north_vector=north_vector, hide_axes=hide_axes, iscolorlog=iscolorlog if iscolorlog else colorlog_dict[d], noweight=noweight, fontsize=fontsize, annotate_markers=annotate_markers, args=args)
 
             if add_velocity: prj.annotate_velocity(factor=20)
             if add_arrow:
@@ -262,7 +265,7 @@ if __name__ == '__main__':
         if args.fullbox: args.galrad = ds.refine_width / 2 # kpc
         center = ds.halo_center_kpc
 
-        annotate_positions = [] ##
+        annotate_markers = [] ##
 
         if not args.noplot:
             start_frame = 0 if args.makerotmovie or (args.rot_normal_by == 0 and args.rot_north_by == 0) else 1
@@ -273,8 +276,8 @@ if __name__ == '__main__':
                 fig = make_projection_plots(ds=refine_box.ds, center=center, \
                                         refine_box=refine_box, box_width=2 * args.galrad * kpc, \
                                         fig_dir=fig_dir, name=halo_dict[args.halo], output=this_sim[1], fontsize=args.fontsize*1.5, \
-                                        fig_end='projection', do=[ar for ar in args.do.split(',')], projections=[ar for ar in args.projection.split(',')], annotate_positions=annotate_positions, \
-                                        is_central=args.do_central, add_arrow=args.add_arrow, add_velocity=args.add_velocity, rot_frame=nrot, \
+                                        fig_end='projection', do=[ar for ar in args.do.split(',')], projections=[ar for ar in args.projection.split(',')], annotate_positions=[], \
+                                        is_central=args.do_central, add_arrow=args.add_arrow, add_velocity=args.add_velocity, rot_frame=nrot, annotate_markers=annotate_markers, \
                                         total_normal_rot=args.rot_normal_by, total_north_rot=args.rot_north_by, rot_north_about=args.rot_north_about, rot_normal_about=args.rot_normal_about, \
                                         nframes=(end_frame - start_frame), hide_axes=args.hide_axes, iscolorlog=args.iscolorlog, noweight=args.noweight, cbar_horizontal=False, use_density_cut=args.use_density_cut, args=args) # using halo_center_kpc instead of refine_box_center
         else:
