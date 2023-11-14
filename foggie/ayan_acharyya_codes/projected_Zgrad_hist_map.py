c4653b62f533678d890231b4996b7075ced55dab
ayanacharyya <acharyyaayan@gmail.com>
2023-11-14 01:14:51 -0500

    consolidated plotting functions for both projected and non-projected plots into a common routine

--- foggie/ayan_acharyya_codes/projected_Zgrad_hist_map.py
+++ foggie/ayan_acharyya_codes/projected_Zgrad_hist_map.py
@@ -12,11 +12,12 @@
 """
 from header import *
 from util import *
-from plot_MZgrad import plot_zhighlight
-from compute_Zscatter import fit_distribution
-from uncertainties import ufloat, unumpy
-from lmfit.models import GaussianModel, SkewedGaussianModel
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+from nonprojected_Zgrad_hist_map import plot_projection, plot_Zprof_snap, plot_Zdist_snap
+# from plot_MZgrad import plot_zhighlight
+# from compute_Zscatter import fit_distribution
+# from uncertainties import ufloat, unumpy
+# from lmfit.models import GaussianModel, SkewedGaussianModel
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 start_time = time.time()
 plt.rcParams['axes.linewidth'] = 1
@@ -108,144 +109,6 @@ def make_df_from_frb(frb, df, projection, args):
 
     return df
 
-# -----------------------------------------------------------------------
-def plot_projection(quantity, box, box_center, box_width, projection, ax, args, unit=None, clim=None, cmap=None):
-    '''
-    Function to plot the 2D map of various velocity quantities, at the given resolution of the FRB
-    :return: axis handle
-    '''
-    myprint('Now making projected velocity quantity plot..', args)
-
-    field = ('gas', field_dict[quantity])
-    weight_field = ('gas', args.weight) if args.weight is not None else None
-
-    prj = yt.ProjectionPlot(box.ds, projection, field, center=box_center, data_source=box, width=box_width, weight_field=weight_field, fontsize=args.fontsize, buff_size=(args.ncells, args.ncells))
-    if unit is not None: prj.set_unit(field, unit)
-    if cmap is not None: prj.set_cmap(field, cmap)
-    if clim is not None: prj.set_zlim(field, zmin=clim[0], zmax=clim[1])
-
-    # ------plotting onto a matplotlib figure--------------
-    position = ax.get_position()
-    prj.plots[field].axes = ax
-    divider = make_axes_locatable(ax)
-    prj._setup_plots()
-    prj.plots[field].axes.set_position(position)  # in order to resize the axis back to where it should be
-
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    cbar = fig.colorbar(prj.plots[field].cb.mappable, orientation='vertical', cax=cax)
-    cbar.ax.tick_params(labelsize=args.fontsize)
-    cbar.set_label(prj.plots[field].cax.get_ylabel(), fontsize=args.fontsize / args.fontfactor if quantity != 'metal' else args.fontsize)
-
-    ax.tick_params(axis='both', labelsize=args.fontsize)
-    ax.set_xlabel(ax.get_xlabel(), fontsize=args.fontsize)
-    ax.set_ylabel(ax.get_ylabel(), fontsize=args.fontsize)
-
-    return ax
-
-# ----------------------------------------------------------------
-def plot_Zprof_snap(df, ax, args):
-    '''
-    Function to plot the radial metallicity profile (from input dataframe) as seen from all three projections, on to the given axis
-    Also computes the projected metallicity gradient along each projection
-    :return: fitted gradient across each projection, and the axis handle
-    '''
-    plt.style.use('seaborn-whitegrid')
-    myprint('Now making the radial profile plot for ' + args.output + '..', args)
-    x_bins = np.linspace(0, args.galrad / args.re if 're' in args.xcol else args.galrad, 10)
-    Zgrad_arr = []
-
-    ycol = 'metal'
-    color = 'blue'
-
-    df['log_metal'] = np.log10(df[ycol])
-    if not args.plot_onlybinned: ax.scatter(df['rad'], df['log_metal'], c='cornflowerblue', s=5, lw=0, alpha=0.7)
-
-    df['binned_cat'] = pd.cut(df['rad'], x_bins)
-
-    if args.weight is not None:
-        agg_func = lambda x: np.sum(x * df.loc[x.index, args.weight]) / np.sum(df.loc[x.index, args.weight]) # function to get weighted mean
-        agg_u_func = lambda x: np.sqrt(((np.sum(df.loc[x.index, args.weight] * x**2) / np.sum(df.loc[x.index, args.weight])) - (np.sum(x * df.loc[x.index, args.weight]) / np.sum(df.loc[x.index, args.weight]))**2) * (np.sum(df.loc[x.index, args.weight]**2)) / (np.sum(df.loc[x.index, args.weight])**2 - np.sum(df.loc[x.index, args.weight]**2))) # eq 6 of http://seismo.berkeley.edu/~kirchner/Toolkits/Toolkit_12.pdf
-    else:
-        agg_func, agg_u_func = np.mean, np.std
-
-    y_binned = df.groupby('binned_cat', as_index=False).agg([(ycol, agg_func)])[ycol].values.flatten()
-    y_u_binned = df.groupby('binned_cat', as_index=False).agg([(ycol, agg_u_func)])[ycol].values.flatten()
-    x_bin_centers = x_bins[:-1] + np.diff(x_bins) / 2
-
-    quant = unumpy.log10(unumpy.uarray(y_binned, y_u_binned)) # for correct propagation of errors
-    y_binned, y_u_binned = unumpy.nominal_values(quant), unumpy.std_devs(quant) # in logspace
-
-    # getting rid of potential nan values
-    indices = np.array(np.logical_not(np.logical_or(np.isnan(x_bin_centers), np.isnan(y_binned))))
-    x_bin_centers = x_bin_centers[indices]
-    y_binned = y_binned[indices]
-    y_u_binned = y_u_binned[indices]
-
-    # ----------to plot mean binned y vs x profile--------------
-    linefit, linecov = np.polyfit(x_bin_centers, y_binned, 1, cov=True)#, w=1. / (y_u_binned) ** 2)  # linear fitting done in logspace
-    y_fitted = np.poly1d(linefit)(x_bin_centers) # in logspace
-
-    Zgrad = ufloat(linefit[0], np.sqrt(linecov[0][0]))
-    Zgrad_arr.append(Zgrad)
-
-    print('Upon radially binning: Inferred slope for halo ' + args.halo + ' output ' + args.output + ' projection ' + thisproj + ' is', Zgrad, 'dex/kpc')
-
-    ax.errorbar(x_bin_centers, y_binned, c=color, yerr=y_u_binned, lw=2, ls='none', zorder=5)
-    ax.scatter(x_bin_centers, y_binned, c=color, s=30, lw=1, ec='black', zorder=10)
-    ax.plot(x_bin_centers, y_fitted, color=color, lw=2.5, ls='dashed')
-
-    ax.set_xlabel('Radius (kpc)', fontsize=args.fontsize)
-    ax.set_ylabel(r'log Metallicity (Z$_{\odot}$)', fontsize=args.fontsize)
-    ax.set_xlim(0, 5. if args.forproposal or args.forpaper else np.ceil(args.upto_kpc / 0.695)) # kpc
-    ax.set_ylim(args.Zlim[0], args.Zlim[1]) # log limits
-    ax.tick_params(axis='both', labelsize=args.fontsize)
-
-    return Zgrad_arr, ax
-
-# ----------------------------------------------------------------
-def plot_Zdist_snap(df, ax, args):
-    '''
-    Function to plot the metallicity histogram (from input dataframe) as seen from all three projections, on to the given axis
-    Also fits the histogram of projected metallicity along each projection
-    :return: fitted histogram parameters across each projection, and the axis handle
-    '''
-    myprint('Now making the histogram plot for ' + args.output + '..', args)
-
-    ycol = 'metal'
-    Zarr = df[ycol].values
-    weights = df[args.weight].values if args.weight is not None else None
-    color = 'blue'
-
-    if args.islog: Zarr = np.log10(Zarr)  # all operations will be done in log
-
-    p = ax.hist(Zarr, bins=args.nbins, histtype='step', lw=1, density=True, ec='cornflowerblue', weights=weights)
-
-    if not args.nofit:
-        Zdist_arr = []
-        fit = fit_distribution(Zarr, args, weights=weights)
-        xvals = p[1][:-1] + np.diff(p[1])
-        ax.plot(xvals, fit.init_fit, c=color, lw=1, ls='--') # for plotting the initial guess
-        #ax.plot(xvals, fit.best_fit, c=color, lw=1)
-        if not args.hide_multiplefit:
-            ax.plot(xvals, GaussianModel().eval(x=xvals, amplitude=fit.best_values['g_amplitude'], center=fit.best_values['g_center'], sigma=fit.best_values['g_sigma']), c=color, lw=1, ls='--', label='Regular Gaussian')
-            ax.plot(xvals, SkewedGaussianModel().eval(x=xvals, amplitude=fit.best_values['sg_amplitude'], center=fit.best_values['sg_center'], sigma=fit.best_values['sg_sigma'], gamma=fit.best_values['sg_gamma']), c=color, lw=1, ls='dotted', label='Skewed Gaussian')
-        Zdist_arr.append([fit.best_values['sg_sigma'], fit.best_values['sg_center']])
-    else:
-        Zdist_arr = np.nan
-
-    ax.set_xlabel(r'log Metallicity (Z$_{\odot}$)' if args.islog else r'Metallicity (Z$_{\odot}$)', fontsize=args.fontsize)
-    ax.set_ylabel('Normalised PDF', fontsize=args.fontsize)
-    ax.set_xlim(args.Zlim[0], args.Zlim[1]) # Zsun
-    ax.set_ylim(0, 2.5 if args.forproposal else 3)
-    ax.tick_params(axis='both', labelsize=args.fontsize)
-    if not args.nofit: ax.text(ax.get_xlim()[1]*0.9, ax.get_ylim()[1]*0.9, 'Center = %.2F\nWidth = %.2F' % (fit.best_values['sg_center'], 2.355 * fit.best_values['sg_sigma']), color='k', transform=ax.transAxes, fontsize=args.fontsize, va='top', ha='left' if args.islog else 'right')
-
-    #ax.text(0.03 if args.islog else 0.97, 0.3, 'z = %.2F' % args.current_redshift, ha='left' if args.islog else 'right', va='top', transform=ax.transAxes, fontsize=args.fontsize)
-    #ax.text(0.03 if args.islog else 0.97, 0.2, 't = %.1F Gyr' % args.current_time, ha='left' if args.islog else 'right', va='top', transform=ax.transAxes, fontsize=args.fontsize)
-
-    return Zdist_arr, ax
-
-
 field_dict = {'vrad': 'radial_velocity_corrected', 'vdisp_3d': 'velocity_dispersion_3d', 'vtan': 'tangential_velocity_corrected', \
               'vphi': 'phi_velocity_corrected', 'vtheta': 'theta_velocity_corrected', 'metal':'metallicity'}
 label_dict = {'vrad': r'$v_{\rm radial}$', 'vdisp_3d': r'3D $\sigma_v$', 'vdisp_los': r'LoS $\sigma_v$', 'vtan': r'$v_{\rm tangential}$', \
@@ -317,7 +180,6 @@ if __name__ == '__main__':
         args.current_redshift = ds.current_redshift
         args.current_time = ds.current_time.in_units('Gyr').tolist()
 
-        args.projections = [item for item in args.projection.split(',')]
         args.Zlim = [-1.5, 0.5] if args.forproposal else [-2, 1]# log Zsun units
         if args.res_arc is not None:
             args.res = get_kpc_from_arc_at_redshift(float(args.res_arc), args.current_redshift)
@@ -350,10 +212,15 @@ if __name__ == '__main__':
         Path(args.fig_dir).mkdir(parents=True, exist_ok=True)
 
         # -------setting up fig--------------
-        nrow, ncol = len(args.projections), 4
-        fig, axes = plt.subplots(nrow, ncol, figsize=(15, 4 if nrow == 1 else 6 if nrow == 2 else 8))
+        nrow, ncol1, ncol2 = 3, 2, 3
+        ncol = ncol1 * ncol2  # the overall figure is nrow x (ncol1 + ncol2)
+        fig = plt.figure(figsize=(8, 8))
+        axes_met_proj = [plt.subplot2grid(shape=(nrow, ncol), loc=(0, int(item)), colspan=ncol1) for item in np.linspace(0, ncol1 * 2, 3)]
+        axes_vel_proj = [plt.subplot2grid(shape=(nrow, ncol), loc=(1, int(item)), colspan=ncol1) for item in np.linspace(0, ncol1 * 2, 3)]
+        ax_prof_snap = plt.subplot2grid(shape=(nrow, ncol), loc=(2, 0), colspan=ncol2)
+        ax_dist_snap = plt.subplot2grid(shape=(nrow, ncol), loc=(2, ncol2), colspan=ncol - ncol2)
         fig.tight_layout()
-        fig.subplots_adjust(top=0.95, bottom=0.07, left=0.07, right=0.93, wspace=0.7, hspace=0.5)
+        fig.subplots_adjust(top=0.9, bottom=0.07, left=0.1, right=0.9, wspace=0.95, hspace=0.35)
 
         # ------tailoring the simulation box for individual snapshot analysis--------
         if args.upto_kpc is not None: args.re = np.nan
@@ -385,6 +252,7 @@ if __name__ == '__main__':
             map_dist = get_dist_map(args)
             df_snap['rad'] = map_dist.flatten()
 
+            # -------loop over lines of sight---------------------
             for thisproj in ['x', 'y', 'z']:
                 field_dict.update({'vdisp_los':'velocity_dispersion_' + thisproj, 'vlos': 'v' + thisproj + '_corrected'})
                 frb = make_frb_from_box(box, box_center, box_width, thisproj, args)
@@ -396,27 +264,24 @@ if __name__ == '__main__':
             myprint('Reading in existing ' + df_snap_filename, args)
             df_snap = pd.read_table(df_snap_filename, delim_whitespace=True, comment='#')
 
-        # -------loop over lines of sight---------------------
-        for index, thisproj in enumerate(args.projections):
-            args.projection = thisproj
-            ax_row = axes if len(args.projections) == 1 else axes[index]
-            columns_to_extract = ['rad', 'metal_' + thisproj]
-            if args.weight is not None: columns_to_extract += [args.weight + '_' + thisproj]
-            df_thisproj = df_snap[columns_to_extract]
-            df_thisproj = df_thisproj.replace([0, np.inf, -np.inf], np.nan).dropna(subset=columns_to_extract, axis=0)
-            df_thisproj.columns = df_thisproj.columns.str.replace('_' + thisproj, '')
+        # ------plotting projected metallcity snapshots---------------
+        axes_met_proj = plot_projection('metal', box, box_center, box_width, axes_met_proj, args, clim=[10 ** -1.5, 10 ** 0] if args.forproposal else None, cmap=old_metal_color_map, ncells=args.ncells)
 
-            # ------plotting projected metallicity profiles---------------
-            Zgrad_arr, ax_row[0] = plot_Zprof_snap(df_thisproj, ax_row[0], args)
+        # ------plotting projected velocity quantity snapshots---------------
+        axes_vel_proj = plot_projection(args.vcol, box, box_center, box_width, axes_vel_proj, args, clim=[-150, 150] if args.vcol == 'vrad' or args.vcol == 'vphi' or args.vcol == 'vlos' else [0, 150] if args.forproposal else None, cmap='PRGn' if args.vcol == 'vrad' else 'viridis', ncells=args.ncells)
 
-            # ------plotting projected metallicity histograms---------------
-            Zdist_arr, ax_row[1] = plot_Zdist_snap(df_thisproj, ax_row[1], args)
+        # -------extracting columns for just the given line of sight---------------------
+        columns_to_extract = ['rad', 'metal_' + args.projection]
+        if args.weight is not None: columns_to_extract += [args.weight + '_' + args.projection]
+        df_thisproj = df_snap[columns_to_extract]
+        df_thisproj = df_thisproj.replace([0, np.inf, -np.inf], np.nan).dropna(subset=columns_to_extract, axis=0)
+        df_thisproj.columns = df_thisproj.columns.str.replace('_' + args.projection, '')
 
-            # ------plotting projected metallcity snapshots---------------
-            ax_row[2] = plot_projection('metal', box, box_center, box_width, thisproj, ax_row[2], args, clim=[10**-1.5, 10**0] if args.forproposal else None, cmap=old_metal_color_map)
+        # ------plotting projected metallicity profiles---------------
+        Zgrad_arr, ax_prof_snap = plot_Zprof_snap(df_thisproj, ax_prof_snap, args)
 
-            # ------plotting projected velocity quantity snapshots---------------
-            ax_row[3] = plot_projection(args.vcol, box, box_center, box_width, thisproj, ax_row[3], args, clim=[-150, 150] if args.vcol == 'vrad' or args.vcol == 'vphi' or args.vcol == 'vlos' else [0, 150] if args.forproposal else None, cmap='PRGn' if args.vcol == 'vrad' else 'viridis')
+        # ------plotting projected metallicity histograms---------------
+        Zdist_arr, ax_dist_snap = plot_Zdist_snap(df_thisproj, ax_dist_snap, args)
 
         # ------saving fig------------------
         outfile_rootname = '%s_%s_projectedZ_prof_hist_map_%s%s%s%s.png' % (args.output, args.halo, args.Zgrad_den, args.upto_text, args.weightby_text, args.res_text)
