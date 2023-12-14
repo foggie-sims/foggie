34216e0821f5c2be3f4fe1ae00138e77df19cc76
Molly Peeples <molly@stsci.edu>
2023-12-14 12:56:29 -0500

    cyclone to 1736

--- foggie/halo_analysis/get_center_track.py
+++ foggie/halo_analysis/get_center_track.py
@@ -229,9 +229,9 @@ if __name__ == "__main__":
         start_snap = 2400
         end_snap = 2520
     elif args.halo == "2878":
-        first_center = [0.5132875442504883, 0.5194501876831055, 0.47979068756103516]
-        start_snap = 1600
-        end_snap = 1700
+        first_center = [0.513667106628418, 0.5202836990356445, 0.47907924652099615]
+        start_snap = 1700
+        end_snap = 1736
     else:
         sys.exit("halo not found!")
 
