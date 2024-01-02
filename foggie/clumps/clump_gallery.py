e3b853ab75f0df2c037de3256ee310088e1ddf75
Ramona <Ramona@ma017433.ads.eso.org>
2024-01-02 15:27:30 -0500

    added file name into plot

--- foggie/clumps/clump_gallery.py
+++ foggie/clumps/clump_gallery.py
@@ -7,9 +7,11 @@ from astropy import units as u
 density_color_map = sns.blend_palette(
     ("black", 'cyan', "#4575b4", "#4daf4a", "#ffe34d", "darkorange"), as_cmap=True)
 
-dataset_name = os.getenv('DROPBOX_DIR') + '/FOGGIE/snapshots/halo_008508/nref11c_nref9f/RD0027/RD0027'
-trackname = os.getenv('DROPBOX_DIR') + '/FOGGIE/foggie/foggie/halo_tracks/008508/nref11n_selfshield_15/halo_track_200kpc_nref10'
-track_dir = os.getenv('DROPBOX_DIR') + '/FOGGIE/foggie/foggie/halo_tracks/008508/nref11n_selfshield_15/'
+#dataset_name = os.getenv('DROPBOX_DIR') + '/FOGGIE/snapshots/halo_008508/nref11c_nref9f/RD0027/RD0027'
+#trackname = os.getenv('DROPBOX_DIR') + '/FOGGIE/foggie/foggie/halo_tracks/008508/nref11n_selfshield_15/halo_track_200kpc_nref10'
+#track_dir = os.getenv('DROPBOX_DIR') + '/FOGGIE/foggie/foggie/halo_tracks/008508/nref11n_selfshield_15/'
+
+
 ds, refine_box = foggie_load(dataset_name, trackname, halo_c_v_name=track_dir + 'halo_c_v', disk_relative=True, particle_type_for_angmom='young_stars')
 
 files = ['shell_level1_20cells_20.0kpc/halo_008508_nref11c_nref9f_RD0027_RD0027_clumps_tree.h5', 
@@ -31,6 +33,6 @@ for l in all_leaves:
         p = yt.ProjectionPlot(ds, axis, 'density', weight_field='density', center=ds.halo_center_code, width=(80, 'kpc'))  
         p.set_cmap('density', density_color_map)
         p.annotate_clumps([l,l], color='white')
-        p.annotate_title('Clump ID = ' + str(l.__dict__['clump_id']) + ", Parent = " + str(l.__dict__['parent_id']) ) 
+        p.annotate_title('Clump ID = ' + str(l.__dict__['clump_id']) + ", Parent = " + str(l.__dict__['parent_id']) + "\n file = " + str(l.__dict__['ds']) )
         p.save(str(l.__dict__['clump_id']) + '_'+str(l.__dict__['parent_id'])) 
 
