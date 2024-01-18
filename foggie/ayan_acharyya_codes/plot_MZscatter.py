68b3c7388da579806eb40fb53cbc8f74e9fd2871
ayanacharyya <acharyyaayan@gmail.com>
2024-01-18 12:35:11 -0500

    modified column names for backward compatibility

--- foggie/ayan_acharyya_codes/plot_MZscatter.py
+++ foggie/ayan_acharyya_codes/plot_MZscatter.py
@@ -34,7 +34,7 @@ def load_df(args):
     df = pd.read_table(dist_filename)
     print('Read in file', dist_filename)
     df.drop_duplicates(subset='output', keep='last', ignore_index=True, inplace=True)
-    df.rename(columns={'Zvar':'Zsigma', 'Zvar_u':'Zsigma_u', 'gauss_mean':'Z2_mean', 'gauss_mean_u':'Z2_mean_u', 'gauss_sigma':'Z2_sigma', 'gauss_sigma_u':'Z2_sigma_u', 'Z2_gamma':'Z2_skew', 'Z2_gamma_u':'Z2_skew_u'}, inplace=True) # for backward compatibility
+    df.rename(columns={'Zvar':'Zsigma', 'Zvar_u':'Zsigma_u', 'gauss_mean':'Z2_mean', 'Zgauss_mean':'Z2_mean', 'gauss_mean_u':'Z2_mean_u', 'Zgauss_mean_u':'Z2_mean_u', 'gauss_sigma':'Z2_sigma', 'Zgauss_sigma':'Z2_sigma', 'gauss_sigma_u':'Z2_sigma_u', 'Zgauss_sigma_u':'Z2_sigma_u', 'Z2_gamma':'Z2_skew', 'Zgauss_gamma':'Z2_skew', 'Z2_gamma_u':'Z2_skew_u', 'Zgauss_gamma_u':'Z2_skew_u'}, inplace=True) # for backward compatibility
 
     # ---------reading in dataframe produced by compute_MZgrad.py-----------
     Zgrad_den_text = 'rad' if args.upto_kpc is not None else 'rad_re'
