c60e230f534a71ca7b8079e502ffeb8d0283fbb3
Claire Kopenhafer <kopenhaf@msu.edu>
2024-12-10 11:44:46 -0500

    Remove unneeded stuff copied from Enzo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,8 +26,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 sys.path.append(os.path.abspath('exts'))
-extensions = ['sphinx.ext.imgmath',  'sphinx.ext.intersphinx',
-              'youtube']
+extensions = ['sphinx.ext.imgmath',  'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -124,7 +123,7 @@ html_theme_path = ['.']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
