5b66c0cad8db8324c782a470bec20893c2c8c9e7
tumlinson@stsci.edu <tumlinson@stsci.edu>
2025-04-07 15:21:49 -0400

    halo_callbacks

--- foggie/utils/halo_quantity_callbacks.py
+++ foggie/utils/halo_quantity_callbacks.py
@@ -1,4 +1,5 @@
 
+import yt 
 
 def halo_average_temperature(halo):
     sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
@@ -7,22 +8,26 @@ def halo_average_temperature(halo):
     if sphere is None:
         return halo.halo_catalog.data_ds.quan(0, "K")
 
-    # use the sphere to calculate average temperature weighted by cell mass
-    # using the weighted_average_quantity derived quantity
     return sphere.quantities.weighted_average_quantity(
         ("gas", "temperature"), ("gas", "cell_mass"))
 
 def halo_average_metallicity(halo):
     sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
 
-    # if the halo was too small, the sphere callback will return a None, then just return 0 (in correct units)
     if sphere is None:
         return halo.halo_catalog.data_ds.quan(0, "dimensionless")
 
-    # use the sphere to calculate average temperature weighted by cell mass
-    # using the weighted_average_quantity derived quantity
     return sphere.quantities.weighted_average_quantity(
         ("gas", "metallicity"), ("gas", "cell_mass"))
+
+def halo_max_metallicity(halo):
+    sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
+
+    if sphere is None:
+        return halo.halo_catalog.data_ds.quan(0, "dimensionless")
+
+    zmax = sphere.quantities.extrema(("gas", "metallicity")) / 0.02 
+    return zmax[1]
 
 def halo_total_gas_mass(halo): 
     sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
@@ -39,6 +44,46 @@ def halo_total_star_mass(halo):
         return halo.halo_catalog.data_ds.quan(0, "Msun")
 
     return sphere.quantities.total_quantity(('stars', 'particle_mass'))
+
+def halo_total_metal_mass(halo): 
+    sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
+
+    if sphere is None:
+        return halo.halo_catalog.data_ds.quan(0, "Msun")
+    
+    return sphere.quantities.total_quantity(('gas', 'metal_mass'))
+
+def halo_total_young_stars7_mass(halo): 
+    sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
+
+    if sphere is None:
+        return halo.halo_catalog.data_ds.quan(0, "Msun")
+
+    return sphere.quantities.total_quantity(('young_stars', 'particle_mass'))
+
+def halo_sfr7(halo): 
+    sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
+
+    if sphere is None:
+        return halo.halo_catalog.data_ds.quan(0, "Msun")
+
+    return sphere.quantities.total_quantity(('young_stars', 'particle_mass')) / yt.YTArray(1e7, 'yr')  
+
+def halo_total_young_stars8_mass(halo): 
+    sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
+
+    if sphere is None:
+        return halo.halo_catalog.data_ds.quan(0, "Msun")
+
+    return sphere.quantities.total_quantity(('young_stars8', 'particle_mass'))
+
+def halo_sfr8(halo): 
+    sphere = halo.data_object    # this sphere will have been made for us by the "sphere" callback
+    
+    if sphere is None:
+        return halo.halo_catalog.data_ds.quan(0, "Msun")
+
+    return sphere.quantities.total_quantity(('young_stars8', 'particle_mass')) / yt.YTArray(1e7, 'yr')  
 
 def halo_average_fH2(halo):
     sphere = halo.data_object    
