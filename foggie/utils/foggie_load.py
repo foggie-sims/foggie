2c1a4d8f2d0ec80f3a22ebf14c113084efab007d
ayanacharyya <acharyyaayan@gmail.com>
2025-10-04 01:57:21 +0200

    modifications in response to updated foggie_load()

--- foggie/utils/foggie_load.py
+++ foggie/utils/foggie_load.py
@@ -40,7 +40,7 @@ def load_sim(args, **kwargs):
     default_halo_c_v_name = catalog_dir + 'halo_c_v'
     halo_c_v_name = kwargs.get('halo_c_v_name', default_halo_c_v_name) # added by Ayan, so that halo_c_v_name can be flexibly passed on to load_sim()
 
-    ds, region = foggie_load(snap_name, trackname, find_halo_center=find_halo_center, halo_c_v_name=halo_c_v_name, disk_relative=disk_relative, \
+    ds, region = foggie_load(snap_name, trackfile_name=trackname, find_halo_center=find_halo_center, halo_c_v_name=halo_c_v_name, disk_relative=disk_relative, \
                             particle_type_for_angmom=particle_type_for_angmom, do_filter_particles=do_filter_particles, \
                             region=region)
 
